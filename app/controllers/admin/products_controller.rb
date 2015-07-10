87eda07c8ac31657289954b08ba56bb6a299a7f5
xdite <xuite.joke@gmail.com>
2015-07-10 15:03:44 +0800

    加入圖片上傳功能

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -5,6 +5,7 @@ class Admin::ProductsController < ApplicationController
 
   def new
     @product = Product.new
+    @photo = @product.photos.new
   end
 
   def create
@@ -39,7 +40,7 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price)
+    params.require(:product).permit(:title, :description, :quantity, :price, :photos_attributes => [:image] )
   end
 
 end
