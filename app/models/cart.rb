97a52e92c953e7bffcd6c289c79a48987b43a9d4
xdite <xuite.joke@gmail.com>
2015-07-12 23:33:05 +0800

    create cart model

---
+++ app/models/cart.rb
@@ -0,0 +1,11 @@
+class Cart < ActiveRecord::Base
+end
+
+# == Schema Information
+#
+# Table name: carts
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710010345) do
+ActiveRecord::Schema.define(version: 20150712153124) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "photos", force: :cascade do |t|
     t.integer  "product_id"
