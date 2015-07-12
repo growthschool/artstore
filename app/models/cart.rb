8ffbdae9ca16ffe2913c23ff267156a7c2b25893
xdite <xuite.joke@gmail.com>
2015-07-12 23:38:33 +0800

    建立 model: cart 與 cart_item 資料庫關聯

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,8 @@
 class Cart < ActiveRecord::Base
+
+  has_many :cart_items, dependent: :destroy
+  has_many :items, through: :cart_items, source: :product
+
 end
 
 # == Schema Information
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,6 @@
 class CartItem < ActiveRecord::Base
+  belongs_to :cart
+  belongs_to :product
 end
 
 # == Schema Information
