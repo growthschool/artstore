d10935746ad07fd43ba0169b6841c1ba82d10727
Reinhardt Lin <reinhardt.lin@gmail.com>
2015-07-13 21:18:10 +0800

    Create product index

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
     resources :products
   end
 
+  resources :products
+
+  root :to => "products#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
