7fe129d2ece5c0cdffb17d6e353e62e4ee5d8d4f
Cyan <wzwkak@gmail.com>
2015-07-13 21:24:13 +0800

    作業7/13

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,33 @@
+class Admin::ProductsController < ApplicationController
+    before_action :authenticate_user!
+
+
+
+    def index
+    end
+    def new
+          @product = Product.new
+            end
+
+      def create
+            @product = Product.new(product_params)
+
+                if @product.save
+                        redirect_to admin_products_path
+                            else
+                                    render :new
+                                        end
+                  end
+
+
+        private
+
+          def product_params
+                params.require(:product).permit(:title, :description, :quantity, :price)
+                  end
+
+                  
+
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+      namespace :admin do
+            resources :products
+              end
+  end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +60,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
