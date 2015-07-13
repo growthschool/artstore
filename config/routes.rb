485532cd1297499cd178c0e8497d231192eb78f3
Vincent <vincent10e@gmail.com>
2015-07-13 20:35:49 +0800

    devise install, and set is_admin to User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  namespace :admin do 
+    resources :products
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +57,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
 end
