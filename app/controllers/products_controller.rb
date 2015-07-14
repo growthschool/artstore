68030ad1eb4f3b9b965913c8847465832d616ed1
ChengEdgar <cgk0910@gmail.com>
2015-07-14 21:18:31 +0800

    交作業

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,10 @@
+class ProductsController < ApplicationController
+ 
+  def index
+    @products = Product.order("id DESC")
+  end
+ 
+  def show
+    @product = Product.find(params[:id])
+  end
+end
