878ab9eb5f603960d879ea64c66e9d634071da69
ericyip <letitbemine@gmail.com>
2015-07-15 03:44:21 +0800

    finish homeworkv1

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,9 +1,19 @@
 module ProductsHelper
-  def render_product_photo(photo)
+  def render_product_photo(photo, size = "thumb")
     if photo.present?
-      image_tag(photo.image.thumb.url, :class => "thumbnail")
+      image_url = photo.image.send(size).url
     else
-      image_tag("http://placehold.it/200x200&text=No Pic", :class => "thumbnail")
-    end
+
+      case size
+      when :medium
+        volume = "300x300"
+      else
+        volume = "200x200"
+      end
+      
+      image_url = "http://placehold.it/#{volume}&text=No Pic"
+      end
+
+      image_tag( image_url, :class => "thumbnail")
   end
 end
