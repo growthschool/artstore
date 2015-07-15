c7826f02a5be8b9fbf09ea6bd4e58832f0171873
xdite <xuite.joke@gmail.com>
2015-07-15 20:33:32 +0800

    build build_item_cache_from_cart function

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,24 @@
 class Order < ActiveRecord::Base
-  belongs_to :user
 
+
+  belongs_to :user
+  has_many :items, :class_name => "OrderItem", :dependent => :destroy
   has_one :info, class_name: "OrderInfo", dependent: :destroy
 
   accepts_nested_attributes_for :info
+
+
+
+  def build_item_cache_from_cart(cart)
+    cart.items.each do |cart_item|
+      item = items.build
+      item.product_name = cart_item.title
+      item.quantity = 1
+      item.price = cart_item.price
+      item.save
+    end
+  end
+    
 end
 
 # == Schema Information
