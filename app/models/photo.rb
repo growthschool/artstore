b3806c8672972ab5dc488426b6ac62fe091244ff
csyo <csyrick@icloud.com>
2015-07-16 23:39:09 +0800

    Bonus: annotate, alert, font-awesome

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: photos
+#
+#  id         :integer          not null, primary key
+#  product_id :integer
+#  image      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Photo < ActiveRecord::Base
   belongs_to :product
 
