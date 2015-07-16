aa1e63fc14581a3b38c87ced4d3cda1fc01de509
oscar <akid83@gmail.com>
2015-07-16 22:03:41 +0800

    homework

--- Gemfile
+++ Gemfile
@@ -23,6 +23,16 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem "devise"
+
+gem "bootstrap-sass"
+
+gem "simple_form"
+
+gem "carrierwave"
+
+gem "mini_magick"
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+
+  namespace :admin do
+    resources :products
+  end
+
+  resources :products
+
+  root :to => "products#index"
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +65,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+  
