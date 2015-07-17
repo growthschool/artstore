4bfe76f49024f58e2d7dc164fcd24292a40ecdd0
Vincent <vincent10e@gmail.com>
2015-07-17 21:33:47 +0800

    add admin/product/edit page

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -20,6 +20,10 @@ class Admin::ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
  
   private
  
