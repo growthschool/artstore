b9dbd1eb94fa8f4287f9949d5de1cec952904755
xdite <xuite.joke@gmail.com>
2015-07-19 22:16:09 +0800

    清空購物車的語法，用 model method 整理包裝成 current_cart.clean!

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -16,6 +16,10 @@ class Cart < ActiveRecord::Base
     cart_items.inject(0) {|sum, item| sum + (item.product.price * item.quantity) }
   end
 
+  def clean!
+    cart_items.destroy_all
+  end
+
 end
 
 # == Schema Information
