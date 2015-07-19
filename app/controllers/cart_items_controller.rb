062d5fac3c77859c94fef657b8a8399026067c51
xdite <xuite.joke@gmail.com>
2015-07-19 17:40:01 +0800

    可以修改購物車內的數量

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -13,4 +13,21 @@ class CartItemsController < ApplicationController
 
   end
 
+  def update
+    @cart = current_cart
+    @item = @cart.cart_items.find(params[:id])
+
+    @item.update(item_params)
+    
+    redirect_to carts_path
+  end
+
+
+  private
+
+  def item_params
+    params.require(:cart_item).permit(:quantity)
+  end
+
+  
 end
