0b8dbbff9dab6b2f821ab1abc7ee6b384af80f10
shawn <lossangel0813@hotmail.com>
2015-07-19 08:38:42 +0800

    create controller products

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,6 @@
+class ProductsController < ApplicationController
+
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
+
+    resources :products
+    root 'products#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
