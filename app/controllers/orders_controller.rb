733bdaf06144abb70d09e8671b132e837b72d3af
xdite <xuite.joke@gmail.com>
2015-07-19 23:13:43 +0800

    付款後跳回使用者後台

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -28,7 +28,7 @@ class OrdersController < ApplicationController
 
     @order.pay!
     
-    redirect_to "/", :notice => "成功完成付款"
+    redirect_to account_orders_path , :notice => "成功完成付款"
   end
  
   private
