193265cf17b7fee10fd45fc75eb8284d8aa35de6
xdite <xuite.joke@gmail.com>
2015-07-19 17:22:21 +0800

    加入數量設計到 cart_items

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150715171035) do
+ActiveRecord::Schema.define(version: 20150719090107) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
     t.integer  "product_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "quantity",   default: 1
   end
 
   create_table "carts", force: :cascade do |t|
@@ -48,13 +49,15 @@ ActiveRecord::Schema.define(version: 20150715171035) do
     t.integer  "user_id"
     t.integer  "total",          default: 0
     t.boolean  "paid",           default: false
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
     t.string   "token"
     t.boolean  "is_paid",        default: false
     t.string   "payment_method"
+    t.string   "aasm_state",     default: "order_placed"
   end
 
+  add_index "orders", ["aasm_state"], name: "index_orders_on_aasm_state"
   add_index "orders", ["token"], name: "index_orders_on_token"
 
   create_table "photos", force: :cascade do |t|
