dd69729bd2d2fcb2daaac41446d641e10ff66ae4
xdite <xuite.joke@gmail.com>
2015-07-19 17:25:39 +0800

    修改 add_to_cart 的方式

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,7 +14,8 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
  
     if !current_cart.items.include?(@product)
-      current_cart.add_product_to_cart(@product)
+      current_cart.add_product_to_cart(@product, 1 )
+
       flash[:notice] = "你已成功將 #{@product.title} 加入購物車"
     else
       flash[:warning] = "你的購物車內已有此物品"
