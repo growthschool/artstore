d4963564a19a0aa6949c6bed571f6c39f814a486
xdite <xuite.joke@gmail.com>
2015-07-20 01:08:48 +0800

    單筆訂單可以改 order 狀態

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -9,4 +9,34 @@ class Admin::OrdersController < ApplicationController
     @orders = Order.recent
   end
 
+  def show
+    @order = Order.find(params[:id])
+    @order_info = @order.info
+    @order_items = @order.items
+  end
+
+  def ship
+    @order = Order.find(params[:id])
+    @order.ship!
+    redirect_to :back
+  end
+
+  def shipped
+    @order = Order.find(params[:id])
+    @order.deliver!
+    redirect_to :back
+  end
+
+  def cancel
+    @order = Order.find(params[:id])
+    @order.cancell_order!
+    redirect_to :back
+  end
+  
+  def return
+    @order = Order.find(params[:id])
+    @order.return_good!
+    redirect_to :back
+  end
+
 end
