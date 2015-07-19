abe4780410b60273aa35d7d7c1dd6d3f0aa2fd30
shawn <lossangel0813@hotmail.com>
2015-07-19 09:12:52 +0800

    simple_form 產品修改

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -20,6 +20,22 @@ class Admin::ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
+
+  end
  
  
   private
