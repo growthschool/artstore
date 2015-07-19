2cda4fd3c32007bff146aa7600b0d4864854a085
shawn <lossangel0813@hotmail.com>
2015-07-19 10:35:10 +0800

    圖片上傳功能

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,10 +1,11 @@
 class Admin::ProductsController < ApplicationController
-
+  
   before_action :authenticate_user!
   before_action :admin_required
  
   def new
     @product = Product.new
+    @photo = @product.photos.new
   end
  
   def create
@@ -16,31 +17,30 @@ class Admin::ProductsController < ApplicationController
       render :new
     end
   end
-
-  def index
-    @products = Product.all
-  end
-
+ 
   def edit
     @product = Product.find(params[:id])
   end
-
-
+ 
   def update
     @product = Product.find(params[:id])
-
-    if @product.update
+ 
+    if @product.update(product_params)
       redirect_to admin_products_path
     else
       render :edit
     end
-
+  end
+  
+  def index
+    @products = Product.all
   end
  
  
   private
  
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price)
+    params.require(:product).permit(:title, :description, :quantity, :price, :photos_attributes => [:image] )
   end
+ 
 end
