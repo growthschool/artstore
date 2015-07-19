303341695056f5d6673bc7fb20f3d76f909eb649
shawn <lossangel0813@hotmail.com>
2015-07-19 17:44:27 +0800

    products#show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,12 @@
 class ProductsController < ApplicationController
 
   def index
+    @products = Product.order("id DESC")
   end
+ 
+  def show
+    @product = Product.find(params[:id])
+  end
+
+
 end
