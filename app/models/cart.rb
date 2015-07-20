ab4e263c58bb286d438370a80a9433174f1847c0
Bear <timfanda35@gmail.com>
2015-07-20 22:01:03 +0800

    Add update cart_item quantity feature

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,8 +2,11 @@ class Cart < ActiveRecord::Base
   has_many :cart_items, dependent: :destroy
   has_many :items, through: :cart_items, source: :product
 
-  def add_product_to_cart(product)
-    items << product
+  def add_product_to_cart(product, amount)
+    cart_item = cart_items.build
+    cart_item.product = product
+    cart_item.quantity = amount
+    cart_item.save
   end
 
   def total_price
