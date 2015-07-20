45476bfad141c23f660c3f572922bf52f327b3b2
Reinhardt Lin <reinhardt.lin@gmail.com>
2015-07-20 20:39:32 +0800

    Add order model

---
+++ app/models/order.rb
@@ -0,0 +1,5 @@
+class Order < ActiveRecord::Base
+  belongs_to :user
+  has_one :info, class_name: "OrderInfo", dependent: :destroy
+  accepts_nested_attributes_for :info
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :orders
+
   root :to => "products#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
