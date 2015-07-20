6fa5100c3101c5a7bce35f44af782defd1e3c2b4
Bear <timfanda35@gmail.com>
2015-07-20 20:03:31 +0800

    Add Cart detail view

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,6 +5,18 @@ class Cart < ActiveRecord::Base
   def add_product_to_cart(product)
     items << product
   end
+
+  def total_price
+    items.inject(0) { |sum, item| sum + item.price }
+
+    # sum = 0
+
+    # items.each do |item|
+    #   sum = sum + item.price
+    # end
+
+    # return sum
+  end
 end
 
 # == Schema Information
