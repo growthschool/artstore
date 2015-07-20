15a1c39e21751357a46a0e5661b22e238c69adb8
Reinhardt Lin <reinhardt.lin@gmail.com>
2015-07-20 19:51:46 +0800

    Add cart controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :carts
+
   root :to => "products#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
