68baa2979ed77149c0c9ed77dccbacfc01454e48
gvszg <zetaentranceserious@gmail.com>
2015-07-20 19:22:49 +0800

    建立購物車

---
+++ app/models/cart.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: carts
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Cart < ActiveRecord::Base
+  has_many :cart_items, dependent: :destroy
+  has_many :items, through: :cart_items, source: :product
+end
