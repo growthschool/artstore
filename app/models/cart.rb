974de6fda0923f17f7391e1da401f8d10079f07b
Bear <timfanda35@gmail.com>
2015-07-20 19:27:41 +0800

    Add Cart and CartItem models

---
+++ app/models/cart.rb
@@ -0,0 +1,17 @@
+class Cart < ActiveRecord::Base
+  has_many :cart_items, dependent: :destroy
+  has_many :items, through: :cart_items, source: :product
+
+  def add_product_to_cart(product)
+    items << product
+  end
+end
+
+# == Schema Information
+#
+# Table name: carts
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
