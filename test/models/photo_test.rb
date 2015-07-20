86d643de06b5e54936459fcd6b99bd0e27879e14
shawn <lossangel0813@hotmail.com>
2015-07-20 09:17:11 +0800

    schema 註記

--- test/models/photo_test.rb
+++ test/models/photo_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: photos
+#
+#  id         :integer          not null, primary key
+#  product_id :integer
+#  image      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class PhotoTest < ActiveSupport::TestCase
