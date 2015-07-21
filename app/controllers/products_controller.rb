2c8de848404f9b288f5a9428853c600071e9f92e
Bear <timfanda35@gmail.com>
2015-07-21 23:28:44 +0800

    Add check product quantity feature

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,9 +11,13 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
 
     if !current_cart.items.include?(@product)
-      current_cart.add_product_to_cart(@product, 1 )
+      if @product.quantity > 0
+        current_cart.add_product_to_cart(@product, 1 )
 
-      flash[:notice] = "你已成功將 #{@product.title} 加入購物車"
+        flash[:notice] = "你已成功將 #{@product.title} 加入購物車"
+      else
+        flash[:warning] = "此物品已停止銷售，你無法將它加入購物車"
+      end
     else
       flash[:warning] = "你的購物車內已有此物品"
     end
