0d3d46b4a25cfe7dc0445e9c3b9dd4d0565fa6ac
gvszg <zetaentranceserious@gmail.com>
2015-07-22 09:02:30 +0800

    能修改購物車內商品數量

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -11,4 +11,17 @@ class CartItemsController < ApplicationController
     redirect_to :back
 
   end
+
+  def update
+    @cart = current_cart
+    @item = @cart.cart_items.find(params[:id])
+    @item.update(item_params)
+    redirect_to carts_path
+  end
+
+
+  private
+    def item_params
+      params.require(:cart_item).permit(:quantity)
+    end
 end
