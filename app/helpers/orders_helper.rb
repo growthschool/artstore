b0ecf6816a874ebcb8ca39aeb0d9b7c36828306e
gvszg <zetaentranceserious@gmail.com>
2015-07-22 17:18:35 +0800

    能改單張訂單狀態

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -6,4 +6,16 @@ module OrdersHelper
   def render_order_state(order)
     t("orders.order_state.#{order.aasm_state}")
   end
+
+  def render_order_options_for_admin(order)
+    render :partial => "admin/orders/state_option", :locals => { :order => order }
+  end
+
+  def render_order_paid_state(order)
+    if order.paid?
+      "已付款"
+    else
+      "未付款"
+    end
+  end
 end
