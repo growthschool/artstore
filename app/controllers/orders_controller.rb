b436bab98508d5e95d8b3670f1886aef88bb96bd
xdite <xuite.joke@gmail.com>
2015-07-23 22:33:01 +0800

    增加 allpay callbacks

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,9 @@
 class OrdersController < ApplicationController
 
- before_action :authenticate_user!
+  before_action :authenticate_user!, except: :allpay_notify
  
+  protect_from_forgery except: :allpay_notify
+
   def create
     @order = current_user.orders.build(order_params)
  
@@ -34,6 +36,18 @@ class OrdersController < ApplicationController
     redirect_to account_orders_path , :notice => "成功完成付款"
   end
  
+  def allpay_notify
+    order = Order.find_by_token(params[:id])
+    type = params[:type]
+
+    if params[:RtnCode] == "1"
+      order.set_payment_with!(type)
+      order.make_payment!
+    end
+
+    render text: '1|OK', status: 200
+  end
+
   private
  
   def order_params
