55c301aeb9c371a5c2f4889eedded90c359e1926
xdite <xuite.joke@gmail.com>
2015-07-24 12:44:08 +0800

    用 Service Object 重新整理 Controller 程式碼

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,10 +9,7 @@ class OrdersController < ApplicationController
  
     if @order.save
       
-      @order.build_item_cache_from_cart(current_cart)
-      @order.calculate_total!(current_cart)
-      current_cart.clean!
-      OrderMailer.notify_order_placed(@order).deliver!
+      OrderPlacingService.new(current_cart, @order).place_order!
 
       redirect_to order_path(@order.token)
     else
