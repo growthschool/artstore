4a70599515b0dc2af851dc27fb4c2c215591fc5f
xdite <xuite.joke@gmail.com>
2015-07-24 12:28:24 +0800

    下單後寄信

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,9 +6,12 @@ class OrdersController < ApplicationController
     @order = current_user.orders.build(order_params)
  
     if @order.save
+      
       @order.build_item_cache_from_cart(current_cart)
       @order.calculate_total!(current_cart)
       current_cart.clean!
+      OrderMailer.notify_order_placed(@order).deliver!
+
       redirect_to order_path(@order.token)
     else
       render "carts/checkout"
