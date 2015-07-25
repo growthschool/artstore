bfa4684b27d900acc2ee8099686148dce9c6d6ba
Cyan <wzwkak@gmail.com>
2015-07-25 16:34:41 +0800

    修改order訂單,配合訂單數量

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,9 +10,9 @@ class Order < ActiveRecord::Base
 	def build_item_cache_from_cart(cart)
     	cart.items.each do |cart_item|
     		item = items.build
-    		item.product_name = cart_item.title
-    		item.quantity = 1
-    		item.price = cart_item.price
+    		item.product_name = cart_item.product.title
+    		item.quantity = cart_item.quantity
+    		item.price = cart_item.product.price
     		item.save
     	end
 	end
