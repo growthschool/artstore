84853591defaf1a046ea3c2886546c0ea4ededb2
LIYINGZHEN <middletonlee1994@gmail.com>
2015-07-26 21:25:59 +0800

    新增－商品被購買後，庫存減少

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,20 +1,7 @@
 class OrdersController < ApplicationController
+  include OrdersHelper
 
- before_action :authenticate_user!
-
-   def create
-     @order = current_user.orders.build(order_params)
-
-     if @order.save
-       @order.build_item_cache_from_cart(current_cart)
-       @order.calculate_total!(current_cart)
-       current_cart.clean!
-       redirect_to order_path(@order.token)
-     else
-       render "carts/checkout"
-     end
-   end
-
+  before_action :authenticate_user!
 
   def show
     @order = Order.find_by_token(params[:id])
@@ -22,17 +9,30 @@ class OrdersController < ApplicationController
     @order_items = @order.items
   end
 
+  def create
+    @order = current_user.orders.new(order_params)
+    if @order.save
+      @order.build_item_cache_from_cart(current_cart)
+      @order.calculate_total!(current_cart)
+      reduce_product_quantity!
+      current_cart.clean!
+      redirect_to order_path(@order.token), notice: "Successfully fill in shipping information"
+    else
+      # render different controller action
+      render "carts/checkout"
+    end
+  end
+
   def pay_with_credit_card
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("credit_card")
-    @order.pay!
-    redirect_to account_orders_path , :notice => "成功完成付款"
+    @order.make_payment!
+    redirect_to "/", :notice => "付款成功"
   end
 
   private
 
   def order_params
-    params.require(:order).permit(:info_attributes => [:billing_name, :billing_address,:shipping_name, :shipping_address] )
+    params.require(:order).permit(info_attributes: [:billing_name, :billing_address,:shipping_name, :shipping_address])
   end
-
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,8 +1,3 @@
-class CartItem < ActiveRecord::Base
-  belongs_to :cart
-  belongs_to :product
-end
-
 # == Schema Information
 #
 # Table name: cart_items
@@ -10,7 +5,21 @@ end
 #  id         :integer          not null, primary key
 #  cart_id    :integer
 #  product_id :integer
+#  quantity   :integer
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  quantity   :integer          default("1")
 #
+# Indexes
+#
+#  index_cart_items_on_cart_id     (cart_id)
+#  index_cart_items_on_product_id  (product_id)
+#
+
+class CartItem < ActiveRecord::Base
+  belongs_to :cart
+  belongs_to :product
+
+  def reduce_quantity
+    self.product.update_attribute(:quantity, (self.product.quantity - self.quantity))
+  end
+end
