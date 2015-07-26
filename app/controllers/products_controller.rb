dc30e429a9460c94c7edd121ff3e54a6de244f6e
Vincent <vincent10e@gmail.com>
2015-07-26 17:46:41 +0800

    customer can't order when the product quantity is 0

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,8 +13,12 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
  
     if !current_cart.items.include?(@product)
-      current_cart.add_product_to_cart(@product, 1)
-      flash[:notice] = "你已成功將 #{@product.title} 加入購物車"
+      if @product.quantity > 0
+        current_cart.add_product_to_cart(@product, 1)
+        flash[:notice] = "你已成功將 #{@product.title} 加入購物車"
+      else
+        flash[:warning] = "此物品已停止銷售，你無法將它加入購物車"
+      end
     else
       flash[:warning] = "你的購物車內已有此物品"
     end
