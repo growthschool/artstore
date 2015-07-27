a2ccdf1beeabace9c970a21a18abb8b0bbd8c27e
hydnwang <onyxlw@yahoo.com.tw>
2015-07-27 21:46:17 +0800

    Services object

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,11 +7,13 @@ class OrdersController < ApplicationController
     @order = current_user.orders.build(order_params)
 
     if @order.save
-      @order.build_item_cache_from_cart(current_cart)
-      @order.calculate_total!(current_cart)
-      # current_cart.cart_items.destroy_all
-      current_cart.clean!
-      OrderMailer.notify_order_placed(@order).deliver!
+      OrderPlacingService(current_cart, order).place_order!
+      # @order.build_item_cache_from_cart(current_cart)
+      # @order.calculate_total!(current_cart)
+      # # current_cart.cart_items.destroy_all
+      # current_cart.clean!
+      # OrderMailer.notify_order_placed(@order).deliver!
+
       redirect_to order_path(@order.token)
     else
       render "carts/checkout"
