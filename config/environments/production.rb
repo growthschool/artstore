f4d7cc88952d1ec7265ea71909ffbb81af13c8ca
Bear <timfanda35@gmail.com>
2015-07-28 01:59:28 +0800

    Change mail from in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Set mail default host
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'bearcave-demo.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port:           587,
