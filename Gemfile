5c3228b31353ba5b3111404005e067fc9db4581b
Bear <timfanda35@gmail.com>
2015-07-27 19:21:47 +0800

    Defualt Mailer

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ group :development, :test do
   gem 'spring'
 
   gem "annotate"
+  gem "letter_opener"
 end
 
 gem "devise"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Use letter_opener to preview mail content
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :letter_opener
 end
