1ed95230b065e607f196d830fe6a300a3e8d6a47
Bear <timfanda35@gmail.com>
2015-07-27 21:46:00 +0800

    Add S3 support to save image

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,14 @@
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV['S3_ACCESS_KEY'], # 你的 key
+      aws_secret_access_key: ENV['S3_SECRET_KEY'], # 你的 secret key
+      region:                ENV['S3_REGION'] # 你的 S3 bucket 的 Region 位置
+    }
+    config.fog_directory  = ENV['S3_BUCKET_NAME'] # 你設定的 bucket name
+  else
+    config.storage :file
+  end
+end
