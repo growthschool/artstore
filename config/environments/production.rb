5441be6fbebae9c34b08007de7ff13fef8d05a2f
gvszg <zetaentranceserious@gmail.com>
2015-07-28 21:01:07 +0800

    fix production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
 
 
 
-config.action_mailer.default_url_options = { host: 'localhost:3000' }
+config.action_mailer.default_url_options = { host: 'obscure-woodland-3425' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port:           587,
