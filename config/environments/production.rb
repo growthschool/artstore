e7b3c50af07844dbe56b5f4bd65ef8aadcdf95ea
gvszg <zetaentranceserious@gmail.com>
2015-07-29 19:08:14 +0800

    fix code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
 
 
 
-config.action_mailer.default_url_options = { host: 'obscure-woodland.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'obscure-woodland.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port:           587,
