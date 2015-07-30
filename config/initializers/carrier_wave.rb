327e20ef343965ba974ff0e17c3c65c6ee02800a
gvszg <zetaentranceserious@gmail.com>
2015-07-30 21:14:46 +0800

    Fix carriwave s3 config

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,10 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:               'AWS',
       aws_access_key_id:      ENV["S3_ID"],
       aws_secret_access_key:  ENV["S3_SECRET"],
-      region:                 'ap-northeast-1'
+      region:                 'us-west-2'
     }
     config.fog_directory =    'forphoto'
   else
