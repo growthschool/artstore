7b35c020de2ade5bfd0b84745b306403fe1ec1d9
sdlong <sdlong.jeng@gmail.com>
2015-09-02 15:49:50 +0800

    新增商品時可以上傳商品圖片

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -9,6 +9,7 @@ class Admin::ProductsController < ApplicationController
 
   def new
     @product = Product.new
+    @photo = @product.photos.new
   end
 
   def edit
@@ -38,6 +39,6 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price)
+    params.require(:product).permit(:title, :description, :quantity, :price, photos_attributes: [:image])
   end
 end
