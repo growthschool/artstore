fdffcafe937e3b629f315f95b80bd87367f6bebe
sdlong <sdlong.jeng@gmail.com>
2015-09-02 11:19:15 +0800

    user 新增 is_admin 欄位

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150902022146) do
+ActiveRecord::Schema.define(version: 20150902025937) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20150902022146) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "is_admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
