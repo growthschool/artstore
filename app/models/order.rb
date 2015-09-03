7702aec4ab32c5da26ac62b67e71eb5a8d73ecd0
sdlong <sdlong.jeng@gmail.com>
2015-09-03 16:23:40 +0800

    將購物車品項資料轉移到 order_items 裡

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,6 +6,16 @@ class Order < ActiveRecord::Base
 
   accepts_nested_attributes_for :info
 
+  def build_item_cache_from_cart(cart)
+    cart.items.each do |cart_item|
+      item = items.build
+      item.product_name = cart_item.title
+      item.quantity = 1
+      item.price = cart_item.price
+      item.save
+    end
+  end
+
   def calculate_total!(cart)
     self.total = cart.total_price
     self.save
