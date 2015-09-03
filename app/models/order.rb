def2b5a9509b9bdbfa8f5dd0d13183c8824b6f6e
sdlong <sdlong.jeng@gmail.com>
2015-09-03 16:13:02 +0800

    訂單的金額欄位來自購物車總價

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,9 @@ class Order < ActiveRecord::Base
   has_one :info, class_name: "OrderInfo", dependent: :destroy
 
   accepts_nested_attributes_for :info
+
+  def calculate_total!(cart)
+    self.total = cart.total_price
+    self.save
+  end
 end
