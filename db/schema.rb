3237d01f3e23710f2eb0239df3df4966d1559b7f
sdlong <sdlong.jeng@gmail.com>
2015-09-04 12:47:33 +0800

    order.rb 新增 payment_method 的欄位
    rails g migration add_payment_method_to_order payment_method:string
    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150904044114) do
+ActiveRecord::Schema.define(version: 20150904044713) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -50,7 +50,8 @@ ActiveRecord::Schema.define(version: 20150904044114) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.string   "token"
-    t.boolean  "is_paid",    default: false
+    t.boolean  "is_paid",        default: false
+    t.string   "payment_method"
   end
 
   add_index "orders", ["token"], name: "index_orders_on_token"
