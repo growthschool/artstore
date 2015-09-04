c2dcad0a986f62627428fb7766508034d501e6f1
sdlong <sdlong.jeng@gmail.com>
2015-09-04 12:41:45 +0800

    order.rb 新增 is_paid 欄位
    rails g migration add_is_paid_to_order is_paid:boolean
    改 default: false
    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150903082514) do
+ActiveRecord::Schema.define(version: 20150904044114) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20150903082514) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.string   "token"
+    t.boolean  "is_paid",    default: false
   end
 
   add_index "orders", ["token"], name: "index_orders_on_token"
