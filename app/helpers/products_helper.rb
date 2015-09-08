43e8b8c9a38a4a1af324f3283948eaedc156dfd4
Yen-Nan Lin <yennanlin.tw@gmail.com>
2015-09-08 11:10:13 +0800

    Add show and index with image page

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,21 @@
 module ProductsHelper
+  def render_product_image(image, size = 'small')
+    image_url = image.nil? ? no_pic_url(size) : image.send(size).url
+    image_tag(image_url, class: 'thumbnail')
+  end
+
+  private
+
+  def no_pic_url(size)
+    volume = case size
+    when 'small'
+      '200x200'
+    when 'medium'
+      '400x400'
+    when 'large'
+      '800x800'
+    end
+
+    'http://placehold.it/' + volume + '&text=No Photo'
+  end
 end
