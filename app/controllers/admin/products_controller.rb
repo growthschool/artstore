1a16304345ffbc6d949f2bdff5edb2797c3c7de4
rugbyball <tzeng.icr915@gmail.com>
2015-09-09 06:58:40 +0000

    toolstore-01

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,50 @@
+class Admin::ProductsController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :admin_required
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+    @photo = @product.photos.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to admin_products_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+    #@product = Product.find(params[:product_id])
+    #@product = Product.find(1)
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :quantity, :price, photos_attributes: [:image])
+  end
+
+
+
+
+end
---
+++ db/migrate/20150907162248_add_is_admin_to_user.rb
@@ -0,0 +1,6 @@
+class AddIsAdminToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :is_admin, :boolean, default: false
+
+  end
+end
