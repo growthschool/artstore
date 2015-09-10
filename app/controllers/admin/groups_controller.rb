20b1ae6a13a699020f676d5c3624ee11069ce127
Elvis Liou <wow.elvis@gmail.com>
2015-09-10 21:40:31 +0800

    FirstWeek HW

---
+++ app/controllers/admin/groups_controller.rb
@@ -0,0 +1,26 @@
+class GroupsController < ApplicationController
+
+before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
+
+
+  def index
+  end
+
+ def show
+ end
+
+ def new
+ end
+
+ def edit
+ end
+
+ def create
+ end
+
+ def update
+ end
+
+ def destroy
+ end
+end
