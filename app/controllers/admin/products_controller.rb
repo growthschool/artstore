91435e1cfd06a64020ead42dd6fd8115ee8ae233
Vincent Chen <vince78718@gmail.com>
2015-09-10 14:40:19 +0800

    新增產品的詳細頁面 - 調整主畫面的 HTML

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -9,6 +9,10 @@ class Admin::ProductsController < ApplicationController
 		@products = Product.all
 	end
 
+	def show
+		@product = Product.find(params[:id])
+	end
+
 	def new
 		@product = Product.new
 		@product.photos.build
