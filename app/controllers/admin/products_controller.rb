b246b72766aec23872d1c2f0df853f77e22b0f8e
weichang <a376836@gmail.com>
2015-09-12 00:52:26 +0800

    新增商品的欄位

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,34 @@
 class Admin::ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(products_params)
+
+    if @product.save
+      redirect_to admin_products_path
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+   def products_params
+      params.require(:product).permit(:title,:description,:price,:quantity)
+   end
 end
