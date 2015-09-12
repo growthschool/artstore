2cda08afe5e6bafcc63b10257f52ff172e6556f8
water.su <pointou@gmail.com>
2015-09-13 00:26:14 +0800

    add admin layout add table layout for admin index add message display helper for product photo add delete button when edit product add shopping cart button css for product-price sort products

--- app/helpers/admin/products_helper.rb
+++ app/helpers/admin/products_helper.rb
@@ -1,2 +1,22 @@
 module Admin::ProductsHelper
+  def render_product_photo(photo, size = "thumb")
+    if photo.present?
+      image_url = photo.image.send(size).url
+    else
+      image_url = render_no_pic_url(size)
+    end
+
+    image_tag(image_url, class: "thumbnail")
+  end
+
+  private
+  def render_no_pic_url(size)
+    case size
+    when :medium
+      volume = "400x400"
+    else
+      volume = "200x200"
+    end
+    return "http://placehold.it/#{volume}&text=本產品無圖片"
+  end
 end
