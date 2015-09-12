d5e1c62644a703e06caff94bdb871bbbdec6dc07
weichang <a376836@gmail.com>
2015-09-12 13:45:22 +0800

    將前台商品頁面圖片改用 helper 呈現

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,11 @@
 module ProductsHelper
+
+  def render_product_photo(photo)
+    if photo.present?
+      image_tag(photo.image.thumb.url, class: "thumbnail")
+    else
+      image_tag("http://placehold.it/200x200&text=No Pic", class: "thumbnail")
+    end
+  end
+
 end
