461dfa5c2f9105e3b452dac1f766bfdc961ee8d6
will <koyasu221b@gmail.com>
2015-09-13 07:39:37 +0800

    upload image

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -6,6 +6,7 @@ class Admin::ProductsController < ApplicationController
     end
     def new
         @product = Product.new
+        @photo = @product.photos.new
     end
 
     def edit
@@ -33,6 +34,10 @@ class Admin::ProductsController < ApplicationController
 
     private
     def product_params
-        params.require(:product).permit(:title, :description, :quantity, :price)
+        params.require(:product).permit(
+            :title, :description,
+            :quantity, :price,
+            photos_attributes: [:image]
+        )
     end
 end
