8899deed03f7c1ea2cf5d451aa48e11dd8ebf0ef
Nelson Tai <nelson@spoonrocket.com>
2015-09-13 21:04:11 +0800

    新增產品的時候可以上傳圖片

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -12,6 +12,7 @@ class Admin::ProductsController < ApplicationController
 
   def new
     @product = Product.new
+    @photo = @product.photos.new
   end
 
   def edit
@@ -41,6 +42,6 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price)
+    params.require(:product).permit(:title, :description, :quantity, :price, photos_attributes: [:image])
   end
 end
