d7de780f7a21f28e1b90b4a2fa11700db5a2c7f4
Nelson Tai <nelson@spoonrocket.com>
2015-09-13 22:27:53 +0800

    Use helper to render product photo

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,24 @@
 module ProductsHelper
+  def render_product_photo(photo, size = "thumb")
+    if photo.present?
+      image_url = photo.image.send(size).url
+    else
+      image_url = render_no_pic_url(size)
+    end
+
+    image_tag(image_url, class: "thumbnail")
+  end
+
+  private
+
+  def render_no_pic_url(size)
+    case size
+    when :medium
+      volumn = "400x400"
+    else
+      volumn = "200x200"
+    end
+
+    return "http://placehold.it/#{volumn}&text=No Pic"
+  end
 end
