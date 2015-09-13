08c9f31c03d8c742856b024028cc411c7de4156a
will <koyasu221b@gmail.com>
2015-09-13 08:47:49 +0800

    change fit to fill for image resize

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -31,16 +31,21 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
+  process resize_to_fit: [800, 800]
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  version :thumb do
+    process :resize_to_fill => [200, 200]
+  end
+
+  version :medium do
+    process :resize_to_fill => [400, 400]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+     %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
