05f50a3266dbfc3b9b36bf2670b6953ebb322cda
Nelson <chiahsien@gmail.com>
2015-09-14 22:04:34 +0800

    上課進度

--- Gemfile
+++ Gemfile
@@ -44,6 +44,8 @@ gem "carrierwave"
 
 gem "mini_magick"
 
+gem "aasm"
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
@@ -54,4 +56,3 @@ group :development, :test do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,17 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+
+    if !current_cart.items.include?(@product)
+      current_cart.add_product_to_cart(@product)
+      flash[:notice] = "已經將 #{@product.title} 加入購物車"
+    else
+      flash[:warning] = "購物車內已經有此產品"
+    end
+
+    redirect_to :back
+  end
 end
