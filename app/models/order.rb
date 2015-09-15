e177db996cf34dbb121c0d28538037411b48548c
Vincent Chen <vince78718@gmail.com>
2015-09-16 00:19:14 +0800

    可以生成訂單

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,26 @@
 class Order < ActiveRecord::Base
 	belongs_to :user
 
+	has_many :items, class_name: "OrderItem", dependent: :destroy
 	has_one :info, class_name: "OrderInfo", dependent: :destroy
 
 	# 有這行才可以用巢狀表單
 	accepts_nested_attributes_for :info
+
+	# 暫存交易紀錄
+	def build_item_cache_from_cart(cart)
+		cart.items.each do |cart_item|
+			item = items.build
+			item.product_name = cart_item.title
+			item.quantity = 1
+			item.price = cart_item.price
+			item.save
+		end
+	end
+
+	# 把總額存在 total 欄位上
+	def calculate_total!(cart)
+		self.total = cart.total_price
+		self.save
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
 		post "checkout", on: :collection # 用 collection 的時候， path 中不用傳類似 :id 這種參數
 	end
 
-	resource :orders, only: [:create]
+	resources :orders, only: [:show, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
