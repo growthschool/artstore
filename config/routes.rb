b7a2202299eb285ae7507e8931664f5ebc679075
Vincent Chen <vince78718@gmail.com>
2015-09-15 17:43:09 +0800

    建立購物車列表頁面 - 新增 carts controller

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
 		end
 	end
 
+	resources :carts, only: [:index] 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
