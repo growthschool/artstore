da31f28d8eae026cc8ce5b8af994e61169d9b595
Springok <wenchuan.lin.tw@gmail.com>
2015-09-16 20:39:55 +0800

    add carts controller and index page

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,10 @@
+class CartsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :carts
+
   root 'products#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
