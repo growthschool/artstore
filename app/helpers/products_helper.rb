ccf02439e1a920423b01af903e92f72efebf59cf
Springok <wenchuan.lin.tw@gmail.com>
2015-09-16 11:26:40 +0800

    add render_product_photo helper

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,12 @@
 module ProductsHelper
+
+  def render_product_photo(photo)
+    if photo.present?
+       image_tag(photo.image.medium.url, class: "thumbnail")
+    else
+       image_tag("http://placehold.it/400x400&text=No Pic", class: "thumbnail")
+    end
+  end
+
+
 end
