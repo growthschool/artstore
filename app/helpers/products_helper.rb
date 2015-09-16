84acab69880be4eb11f2e28e34c771c55b114997
Springok <wenchuan.lin.tw@gmail.com>
2015-09-16 11:56:17 +0800

    modify render_product_photo helper for index and show page

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,12 +1,32 @@
 module ProductsHelper
 
-  def render_product_photo(photo)
+  def render_product_photo(photo, size = "thumb")
+
     if photo.present?
-       image_tag(photo.image.medium.url, class: "thumbnail")
+       image_url = photo.image.send(size).url
     else
-       image_tag("http://placehold.it/400x400&text=No Pic", class: "thumbnail")
+       image_url = render_no_pic_url(size)
     end
+
+
+    image_tag(image_url, class: "thumbnail")
   end
 
+  private
+
+  def render_no_pic_url(size)
+
+    case size
+
+    when :medium
+      volume = "300x300"
+    else :thumb
+      volume = "200x200"
+    end
+
+    return "http://placehold.it/#{volume}&text=No Pic"
+
+  end
 
 end
+
