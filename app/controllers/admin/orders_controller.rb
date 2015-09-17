d9ba4b9ae570ba4de481b4fad78fa3eae2105b96
Fisher Liang <swimnose@gmail.com>
2015-09-17 17:19:40 +0800

    Change order state by state machine

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,12 +1,39 @@
 class Admin::OrdersController < ApplicationController
 	before_action :authenticate_user!
 	before_action :admin_required
+	before_action :find_order, except: [:index]
 
 	def index
 		@orders = Order.all.order("created_at DESC")
 	end
 
 	def show
-		@order = Order.find(params[:id])
+		@order_info = @order.info
+		@order_items = @order.items
 	end
+
+	def ship
+		@order.ship!
+		redirect_to :back
+	end
+
+	def shipped
+		@order.deliver!
+		redirect_to :back
+	end
+
+	def cancel
+		@order.cancel_order!
+		redirect_to :back
+	end
+
+	def return
+		@order.return_good!
+		redirect_to :back
+	end
+
+	private
+		def find_order
+			@order = Order.find(params[:id])
+		end
 end
