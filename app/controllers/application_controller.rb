a46bc6ca9bb37dbd2e28f7c34912b9df916152a0
will <koyasu221b@gmail.com>
2015-09-19 20:08:29 +0800

    add current_cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,23 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_cart
+
+  def current_cart
+      @current_cart ||= find_cart
+  end
+
+  def find_cart
+      cart = Cart.find_by(id: session[:cart_id])
+
+      unless cart.present?
+          cart = Cart.create
+      end
+
+      session[:cart_id] = cart.id
+      cart
+  end
+
   def admin_required
     if !current_user.admin?
       redirect_to "/"
