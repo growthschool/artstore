6a0b0cfeab946ce0702050928da84db1f61b8a75
will <koyasu221b@gmail.com>
2015-09-20 20:14:41 +0800

    redirect to page with token

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,14 +7,16 @@ class OrdersController < ApplicationController
         if @order.save
             @order.build_item_cache_from_cart(current_cart)
             @order.calculate_total!(current_cart)
-            redirect_to order_path(@order)
+            #redirect_to order_path(@order)
+            redirect_to order_path(@order.token)
         else
             render "carts/checkout"
         end
     end
 
     def show
-        @order = Order.find(params[:id])
+        #@order = Order.find(params[:id])
+        @order = Order.find_by_token(params[:id])
         @order_info = @order.info
         @order_items = @order.items
     end
