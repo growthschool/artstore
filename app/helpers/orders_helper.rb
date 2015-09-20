b33167fb9b969043f1d0b75ffe877cb8dc475a58
Springok <wenchuan.lin.tw@gmail.com>
2015-09-20 20:33:24 +0800

    add i18

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,7 +1,11 @@
 module OrdersHelper
 
-def render_order_timestamp(order)
-  order.created_at.to_s(:long)
-end
+  def render_order_timestamp(order)
+    order.created_at.to_s(:long)
+  end
+
+  def render_order_state(order)
+    t("orders.order_state.#{order.aasm_state}") #i18
+  end
 
 end
