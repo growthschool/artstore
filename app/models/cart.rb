8379db18c1785630f3368493b9fb997d7828269f
Vincent Chen <vince78718@gmail.com>
2015-09-21 13:42:20 +0800

    購物車項目可以設定數量。修改總和的計算方法，加入數量的計算

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,12 +2,15 @@ class Cart < ActiveRecord::Base
 	has_many :cart_items, dependent: :destroy # 一個購物車可以有好幾個 items
 	has_many :items, through: :cart_items, source: :product # 透過 cart_items, 取得各自 cart_item 的 product 成為 items
 
-	def add_product_to_cart(product)
-		items << product
+	def add_product_to_cart(product, quantity = 1)
+		cart_item = self.cart_items.build
+		cart_item.product = product
+		cart_item.quantity = quantity
+		cart_item.save
 	end
 
 	def total_price
 		# 預設值為 0 開始計算
-		items.inject(0) { |sum, item| sum + item.price }
+		cart_items.inject(0) { |sum, item| sum + item.total }
 	end
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,8 @@
 class CartItem < ActiveRecord::Base
 	belongs_to :cart
 	belongs_to :product
+
+	def total
+		self.product.price * self.quantity
+	end
 end
