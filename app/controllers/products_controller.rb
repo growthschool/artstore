dfc32ca744273257dde6623e9caf41e1cd250bca
Vincent Chen <vince78718@gmail.com>
2015-09-24 15:55:56 +0800

    修正打錯字

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,7 +11,7 @@ class ProductsController < ApplicationController
 		@product = Product.find(params[:id])
 
 		if !current_cart.items.include?(@product)
-			if @prodcut.has_inventory
+			if @product.has_inventory?
 				current_cart.add_product_to_cart(@product)
 				flash[:notice] = "你已經成功將 #{@product.title} 加入購物車"
 			else
