9313b2169b2ee43f80bb3403786bf5ff8b0eda5c
will <koyasu221b@gmail.com>
2015-09-25 08:27:45 +0800

    refactoring find_cart_item

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -13,4 +13,8 @@ class Cart < ActiveRecord::Base
     def clean!
         cart_items.destroy_all
     end
+
+    def find_cart_item(product)
+        cart_items.find_by(product_id: product)
+    end
 end
