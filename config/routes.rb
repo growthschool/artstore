d12b3a6dfa1ea82c2ff89989edf751f3be9c6222
weichang <a376836@gmail.com>
2015-09-26 15:37:56 +0800

    顯示購物車明細

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,9 @@ Rails.application.routes.draw do
     end
 
   end
-
   root 'products#index'
+
+  resources :carts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
