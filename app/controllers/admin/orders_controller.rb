989bab0145c3d9eea32775c4efdc7bde6c52c6c1
Springok <wenchuan.lin.tw@gmail.com>
2015-09-27 17:12:56 +0800

    add admin/show

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,5 +7,11 @@ class Admin::OrdersController < ApplicationController
   @orders = Order.recent
   end
 
+  def show
+  @order = Order.find(params[:id])
+  @order_info = @order.info
+  @order_items = @order.items
+  end
+
 
 end
--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -7,5 +7,13 @@ module OrdersHelper
     t("orders.order_state.#{order.aasm_state}")
   end
 
+  def render_order_paid_state(order)
+    if order.is_paid?
+      "未付款"
+    else
+      "已付款"
+    end
+
+  end
 
 end
