98ca7ac8b6bb98ba7e34e55cbdc4c62af3f77571
Springok <wenchuan.lin.tw@gmail.com>
2015-09-27 17:21:47 +0800

    add admin order_state modification feature

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -13,5 +13,29 @@ class Admin::OrdersController < ApplicationController
   @order_items = @order.items
   end
 
+  def ship
+    @order = Order.find(params[:id])
+    @order.ship!
+    redirect_to :back
+  end
+
+  def shipped
+    @order = Order.find(params[:id])
+    @order.deliver!
+    redirect_to :back
+  end
+
+  def cancel
+    @order = Order.find(params[:id])
+    @order.cancell_order!
+    redirect_to :back
+  end
+
+  def return
+    @order = Order.find(params[:id])
+    @order.return_good!
+    redirect_to :back
+  end
+
 
 end
--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -9,9 +9,9 @@ module OrdersHelper
 
   def render_order_paid_state(order)
     if order.is_paid?
-      "未付款"
-    else
       "已付款"
+    else
+      "未付款"
     end
 
   end
