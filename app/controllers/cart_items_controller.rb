852892525ddc7b1a4d55b42f609b1cc1516e3289
Springok <wenchuan.lin.tw@gmail.com>
2015-09-27 15:18:03 +0800

    add quantity validation for checkout

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,5 +1,5 @@
 class CartItemsController < ApplicationController
-before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def destroy
     @cart = current_cart
@@ -16,17 +16,20 @@ before_action :authenticate_user!
     @cart = current_cart
     @item = @cart.cart_items.find_by(product_id: params[:id])
 
-    @item.update(item_params)
+    if @item.product.quantity >= item_params[:quantity].to_i
 
-    redirect_to carts_path
+      @item.update(item_params)
+      flash[:notice] = "成功變更數量"
+    else
+      flash[:warning] = "數量不足以加入購物車"
+    end
+      redirect_to carts_path
+    end
+
+    private
+
+    def item_params
+      params.require(:cart_item).permit(:quantity)
+    end
 
   end
-
-
-private
-
-  def item_params
-    params.require(:cart_item).permit(:quantity)
-  end
-
-end
