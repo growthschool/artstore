578db3f6c9b86178b8ff0a22537e3b178a716125
kitb <k5953837@gmail.com>
2015-09-28 17:20:54 +0800

    Added cart item quantity function

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150914135746) do
+ActiveRecord::Schema.define(version: 20150928090334) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
     t.integer  "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "quantity"
   end
 
   create_table "carts", force: :cascade do |t|
