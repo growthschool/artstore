596a50c0223912c9aee3d81ebe1f0c2d81789fc9
Nelson Tai <nelson@spoonrocket.com>
2015-09-28 12:06:54 +0800

    Add item quantity into consideration

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -10,4 +10,24 @@ class CartItemsController < ApplicationController
     flash[:warning] = "成功將 #{@product.title} 從購物車移除"
     redirect_to :back
   end
+
+  def update
+    @cart = current_cart
+    @item = @cart.find_cart_item(params[:id])
+
+    if @item.product.quantity >= item_params[:quantity].to_i
+      @item.update(item_params)
+      flash[:notice] = "成功變更數量"
+    else
+      flash[:warning] = "數量不足以加入購物車"
+    end
+
+    redirect_to carts_path
+  end
+
+  private
+
+  def item_params
+    params.require(:cart_item).permit(:quantity)
+  end
 end
