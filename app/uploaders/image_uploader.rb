be77785d1d274baad74564dc59449d95b265735f
Yen-Nan Lin <yennanlin.tw@gmail.com>
2015-10-01 01:21:28 +0800

    Fix typo in image_uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,7 +7,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  Rails.env.production? ? storage :fog : storage :file
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
