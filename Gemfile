c9f43fd0bb08f64947fb9c097c0d711adf486540
Nelson <chiahsien@gmail.com>
2015-09-30 20:25:37 +0800

    上課進度

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.2'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+# gem 'sqlite3'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -46,6 +46,19 @@ gem "mini_magick"
 
 gem "aasm"
 
+group :development do
+  gem "sqlite3"
+  gem "letter_opener"
+end
+
+group :production do
+  gem "pg"
+  gem "rails_12factor"
+end
+
+gem "activemerchant",         "~> 1.43.1"
+gem "active_merchant_allpay", "0.1.2"
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
@@ -56,4 +69,3 @@ group :development, :test do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
--- app/helpers/account/orders_helper.rb
+++ app/helpers/account/orders_helper.rb
@@ -1,2 +1,35 @@
 module Account::OrdersHelper
+  def render_payment_service_button(order, payment_type)
+    payment_service_for order,
+      order.user.email,
+      service: :allpay,
+      html: { authenticity_token: false, enforce_utf8: false, method: :post } do |service|
+
+      service.merchant_trade_no   "#{order.id}s#{Time.now.strftime("%Y%m%d%H%M%S")}"
+      service.merchant_trade_date order.created_at
+      service.total_amount        order.total
+      service.trade_desc          "order_sample"
+      service.item_name           "order_sample"
+      service.choose_payment      payment_gateway_type(payment_type)
+      service.client_back_url     root_url
+      service.return_url          allpay_notify_order_url(order.token, type: payment_type)
+      service.encrypted_data
+
+      submit_tag t("orders.payment_type.#{payment_type}") , name: nil, class: "btn btn-lg btn-danger btn-group"
+    end
+  end
+
+
+  private
+
+  def payment_gateway_type(payment_type)
+    case payment_type
+    when "credit_card"
+      ActiveMerchant::Billing::Integrations::Allpay::PAYMENT_CREDIT_CARD
+    when "cvs"
+      ActiveMerchant::Billing::Integrations::Allpay::PAYMENT_CVS
+    when "atm"
+      ActiveMerchant::Billing::Integrations::Allpay::PAYMENT_ATM
+    end
+  end
 end
