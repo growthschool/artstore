3e1cec86b0f13eb65e27be60887cdd396c6c00c2
Vincent Chen <vince78718@gmail.com>
2015-10-03 19:49:33 +0800

    Setup mailgun settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+	config.action_mailer.default_url_options = { host: 'localhost:3000' }
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.smtp_settings = {
+		port:           587,
+		address:        "smtp.mailgun.org",
+		user_name:      ENV["MAILGUN_USERNAME"], 
+		password:       ENV["MAILGUN_PASSWORD"],
+		domain:         ENV["MAILGUN_DOMAIN"],
+		authentication: :plain,
+	}
 end
