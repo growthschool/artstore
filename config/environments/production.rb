0c678575f573ab80ce392655f12231f28ad81417
Vincent Chen <vince78718@gmail.com>
2015-10-03 20:28:56 +0800

    修正 mail 裡面回連的 base URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-	config.action_mailer.default_url_options = { host: 'localhost:3000' }
+	config.action_mailer.default_url_options = { host: "artstore-vc7.herokuapp.com" }
 	config.action_mailer.delivery_method = :smtp
 	config.action_mailer.smtp_settings = {
 		port:           587,
