7221f82a977bcf234358d7225ed9de565a54f886
Vincent Chen <vince78718@gmail.com>
2015-10-05 00:21:04 +0800

    修正 S3 上傳失敗，加上 storage 指定成 fog ，將機器位置替換成環境變數方便修改

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,10 +4,11 @@ CarrierWave.configure do |config|
 			provider: "AWS",
 			aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
 			aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-			region: "Tokyo"
+			region: ENV["AWS_REGION"] 
 		}
 		# directory 設定成 S3 上的 bucket 名稱即可 
 		config.fog_directory = ENV["AWS_BUCKET"]
+		config.storage :fog
 	else
 		config.storage :file
 	end
