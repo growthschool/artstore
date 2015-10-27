6544161c0e9283c098c20fe036042758b5ae6b30
sdlong <sdlong.jeng@gmail.com>
2015-10-27 12:09:12 +0800

    消費者可以「手動」從購物車內刪掉某些物品

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,8 @@ Rails.application.routes.draw do
      end
    end
 
+  resources :items, controller: "cart_items"
+
   root "products#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
