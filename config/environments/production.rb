8278a5ef8447b973b8f60f80496c3f934e17e751
sdlong <sdlong.jeng@gmail.com>
2015-10-28 15:05:53 +0800

    設定 default_url & 暫時移除訂單建立時寄信的功能

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'limitless-plains-4640.herokuapp.com' }
+
   config.after_initialize do
     Pay2go.integration_mode = :production
   end
