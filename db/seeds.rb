6f90aa253f01ec01f93f5db957e318448907829f
WillYou <=>
2015-11-09 12:55:49 +0000

    impl login

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+u = User.new
+u.email = "admin@bindo.com"
+u.password = "02040204"
+u.password_confirmation = "02040204"
+u.is_admin = true
+u.save
