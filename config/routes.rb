71681e12b9803867a66387018aa0e17230c53e02
WillYou <=>
2015-11-09 12:25:58 +0000

    step1

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  namespace :admin do
+    resources :products
+  end
+
   # Example resource route with options:
   #   resources :products do
   #     member do
