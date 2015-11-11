4bdc27ffb02afd80964aba8efde1e8cf3cd371f2
haneric <haneric1986@gmail.com>
2015-11-11 11:40:00 +0800

    編輯產品時會因為覆蓋掉上傳圖片是因為 strong parameter 少傳了 id

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -38,6 +38,6 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price, photo_attributes: [:image])
+    params.require(:product).permit(:title, :description, :quantity, :price, photo_attributes: [:image, :id])
   end
 end
