92abb95ba2fae57623c1a090d4f9074115279c1f
Wei-Cheng Yang <rainyday309@gmail.com>
2015-11-12 17:35:42 +0800

    week 1 home 3: finish CRUD

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -19,7 +19,30 @@ class Admin::ProductsController < ApplicationController
 		else
 			render :new
 		end
+	end
 
+	def show
+		@product = Product.find(params[:id])
+	end
+
+	def edit
+		@product = Product.find(params[:id])
+	end
+
+	def update
+		@product = Product.find(params[:id])
+
+		if @product.update(product_params)
+			redirect_to admin_products_path
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@product = Product.find(params[:id])
+		@product.destroy
+		redirect_to admin_products_path
 	end
 
 	private
