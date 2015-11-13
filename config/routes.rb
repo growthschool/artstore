73d50c2874ecd79b8830e0109c2c8dbb12a5c4e8
haneric <haneric1986@gmail.com>
2015-11-13 11:53:15 +0800

    增加管理者切換使用者權限功能以及使用者表單

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :admin do
     resources :products
- end
+    resources :users do
+      post :to_admin
+      post :to_normal
+    end
+  end
 
   resources :products
   root 'products#index'
