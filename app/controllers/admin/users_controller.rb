6e10b72811f9982f44dfd3a0577218c9ad2732a5
wesleychu <jrubyuncle@gmail.com>
2015-11-13 10:00:53 +0800

    [extra-hw] in admin/users_controller.rb, call layout and before_action

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,9 @@
 class Admin::UsersController < ApplicationController
+  layout :admin
+
+  before_action :authenticate_user!
+  before_action :admin_required
+
   def index
     @users = User.all
   end
