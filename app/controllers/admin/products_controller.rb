f2d8b4071dd55b148d7600b91faa0d386df6918e
ycliang <ycliang@ycliangMBPR.local>
2015-11-15 14:30:06 +0800

    Add upload product picture.

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+  layout "admin"
+  
   before_action :authenticate_user!
   before_action :admin_required
   
@@ -8,9 +10,25 @@ class Admin::ProductsController < ApplicationController
 
   def new
     @product = Product.new
+    @photo = @product.build_photo
   end
 
-   def create
+  def edit
+    @product = Product.find(params[:id])
+    @photo = @product.photo
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
+  end
+
+  def create
     @product = Product.new(product_params)
 
     if @product.save
@@ -23,7 +41,7 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price)
+    params.require(:product).permit(:title, :description, :quantity, :price, photo_attributes: [:image, :id])
   end
 
 end
