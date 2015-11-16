81ecb9fdb3a6decac72be42b16967ff3958cdf1e
wesleychu <jrubyuncle@gmail.com>
2015-11-16 21:12:37 +0800

    payment

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,6 +6,11 @@ class Order < ActiveRecord::Base
 
   accepts_nested_attributes_for :info
 
+   before_create :generate_token
+   def generate_token
+     self.token = SecureRandom.uuid
+   end
+
   def build_item_cache_from_cart(cart)
     cart.items.each do |cart_item|
       item = items.build
@@ -20,4 +25,12 @@ class Order < ActiveRecord::Base
     self.total = cart.total_price
     self.save
   end
+
+  def set_payment_with!(method)
+    self.update_columns(payment_method: method )
+  end
+  def pay!
+    self.update_columns(is_paid: true )
+  end
+
 end
