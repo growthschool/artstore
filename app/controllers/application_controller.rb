a3e5051f930447528c941011f2a638cd3686e8d1
AndyWang <andywang@andyMBP.local>
2015-11-16 16:38:21 +0800

    商品加到購物車

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,28 @@ class ApplicationController < ActionController::Base
       redirect_to "/"
     end
   end
+
+   # 若要讓View 也能使用，就要加上helper_method的設定
+  helper_method :current_cart
+
+  # 如果 @current 不為空, 回傳@current_cart
+  def current_cart
+    @current_cart ||= find_cart  # 若這個值是空的話, 就去執行find_cart這個method
+  end
+
+  private
+
+  def find_cart
+    # 首先去找現在使用者的session有沒有cart_id這個值，這樣就可以找到現在這個使用者的購物車
+    cart = Cart.find_by(id: session[:cart_id])
+
+    # 如果找不到，就幫他創造一個購物車
+    unless cart.present?
+      cart = Cart.create
+    end
+
+    # 再重新設定 session[:card_id]的值
+    session[:cart_id] = cart.id
+    cart
+  end
 end
