ba826caba59df76521f889c6d3b6ad2484ce4f11
AndyWang1116 <andywang1116@gmail.com>
2015-11-17 21:16:01 +0800

    購物車加入數量(數量為0不能購買)

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -14,7 +14,13 @@ def update
   @cart = current_cart
   @item = @cart.cart_items.find_by(product_id: params[:id])
 
+  if @item.product.quantity >= item_params[:quantity].to_i  # 在頁面選擇數量時form_for f.select存入的是string, 這裏用to_i比較保險
   @item.update(item_params)
+  flash[:notice] = "成功變更數量"
+  else
+    flash[:warning] = "數量不足以加入購物車"
+  end
+
   redirect_to carts_path
 end
 
