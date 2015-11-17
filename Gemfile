bc2fc36e8ce47cd80ffe65df9f7214339362be1c
latisoft <allen.latitude@gmail.com>
2015-11-17 11:02:38 +0800

    day2: finished

--- Gemfile
+++ Gemfile
@@ -44,6 +44,10 @@ gem "mini_magick"
 
 gem "font-awesome-rails"
 
+gem "aasm"
+
+gem "awesome_rails_console"
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
---
+++ app/models/cart.rb
@@ -0,0 +1,21 @@
+class Cart < ActiveRecord::Base
+
+	has_many :cart_items, dependent: :destroy
+	has_many :items, through: :cart_items, source: :product
+
+  def add_product_to_cart(product)
+    items << product
+  end
+
+  def total_price
+    items.inject(0) { |sum, item| sum + item.price }
+
+# items.each do |item|
+#   sum = sum + item.price
+# end
+# return sum
+
+
+  end
+
+end
