326712ba4f6979d0019b7ec6a229c806cd39604e
AndyWang1116 <andywang1116@gmail.com>
2015-11-17 20:52:21 +0800

    購物車加入數量（基礎設定)

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -10,4 +10,18 @@ def destroy
   redirect_to :back
 end
 
+def update
+  @cart = current_cart
+  @item = @cart.cart_items.find_y(product_id: params[:id])
+
+  @item.update(item_params)
+  redirect_to carts_path
+end
+
+private
+
+def item_params
+  params.require(:cart_item).permit(:quantity)
+
+
 end
