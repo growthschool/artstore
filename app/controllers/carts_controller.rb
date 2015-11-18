d7f35ae43f4891240b8c10e0c3e9812e02566920
AlvinChiu <alvinhsian@gmail.com>
2015-11-18 20:19:17 +0800

    設定訂單資訊頁

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,7 +2,7 @@ class CartsController < ApplicationController
   before_action :authenticate_user!, only: [:checkout]
 
   def checkout
-    @order = current_cart.orders.build
+    @order = current_user.orders.build
     @info = @order.build_info
   end
 end
