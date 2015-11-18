a6a59eb2c68b2fcd584c7836873944a7d75817ab
AlvinChiu <alvinhsian@gmail.com>
2015-11-18 14:49:28 +0800

    [完成] 加入變更管理者權限功能

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :admin do
     resources :products
+    resources :users do
+      member do
+        post :to_admin
+        post :to_normal
+      end
+    end
+  end
+
+  namespace :admin do
+    resources :users
   end
 
   resources :products
