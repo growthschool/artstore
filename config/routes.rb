605859a85a8a9dcd79dd4e8cf1d7ca4005454f49
Wei-Cheng Yang <rainyday309@gmail.com>
2015-11-18 20:10:23 +0800

    建立訂單寄送資訊

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,13 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :carts 
+  resources :carts do
+    collection do
+      post :checkout
+    end
+  end
+
+  resources :orders
   
   root "products#index"
 
