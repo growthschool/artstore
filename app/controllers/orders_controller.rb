e943c8f6dc4be8b85e30be7e82a3311df195b18c
wesleychu <jrubyuncle@gmail.com>
2015-11-18 17:24:28 +0800

    產生訂單後，購物車應該被清空 ref: https://github.com/sdlong/artstore/commit/3c2aef717145b7245c61829faf127a9d29e940c8

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,6 +7,9 @@ class OrdersController < ApplicationController
     if @order.save
       @order.build_item_cache_from_cart(current_cart)
       @order.calculate_total!(current_cart)
+
+      current_cart.clean! # destroy cart_items
+
       redirect_to order_path(@order.token)
     else
       render "carts/checkout"
