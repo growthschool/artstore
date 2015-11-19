ff06bd82822211e0cc934a3ce1412fea15112d86
Wei-Cheng Yang <rainyday309@gmail.com>
2015-11-19 19:59:34 +0800

    清空購物車功能

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,4 +5,10 @@ class CartsController < ApplicationController
 		@order = current_user.orders.build
 		@info = @order.build_info
 	end
+
+	def clean
+		current_cart.clean!
+		flash[:warning] = "已清空購物車"
+		redirect_to carts_path
+	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,7 @@ u.password = "12345678"
 u.password_confirmation = "12345678"
 u.is_admin = true
 u.save
+
+for i in 1..10 do
+	Product.create(title: "Pd#{i}", description: "xxx", quantity: i+10, price: i)
+end
