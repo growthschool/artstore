cc3b68734e6422180eff13b0e5ea98837e845c35
wesleychu <jrubyuncle@gmail.com>
2015-11-19 20:48:22 +0800

    單張 order 應要可以依「按照狀態圖」改變狀態 ref: https://github.com/sdlong/artstore/commit/ea6a91c50a4b754fb96eb0843d6d5816ffad7db2

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -14,9 +14,27 @@ class Admin::OrdersController < ApplicationController
     @order_items = @order.items
   end
 
+  def ship
+    @order = Order.find(params[:id])
+    @order.ship!
+    redirect_to :back
+  end
+
+  def shipped
+    @order = Order.find(params[:id])
+    @order.deliver!
+    redirect_to :back
+  end
+
   def cancel
     @order = Order.find(params[:id])
     @order.cancel_order!
+    redirect_to :back
+  end
+
+  def return
+    @order = Order.find(params[:id])
+    @order.return_good!
     redirect_to :back
   end
 end
