cdea6f543576c4f7006dad9f3c2e138328090198
wesleychu <jrubyuncle@gmail.com>
2015-11-19 14:43:36 +0800

    結賬後轉到 account/orders#index ref: https://github.com/sdlong/artstore/commit/ed04204f06825bb417c343008e9edff3ba25a04f

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -30,7 +30,8 @@ class OrdersController < ApplicationController
     #@order.pay!
     @order.make_payment!
 
-    redirect_to "/", notice: "成功完成付款"
+    #redirect_to "/", notice: "成功完成付款"
+    redirect_to account_orders_path, notice: "成功完成付款"
   end
 
   private
