84c9d76f27798f79a6533cebf42c8d1da93998cc
AndyWang1116 <andywang1116@gmail.com>
2015-11-19 12:38:20 +0800

    建立後台訂單頁面

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -2,4 +2,12 @@ module OrdersHelper
   def render_order_state(order)
     t("orders.order_state.#{order.aasm_state}")  # 使用t function, i18n機制 , 到config/application.rb更改預設語言
   end
+
+  def render_order_paid_state(order)
+    if order.paid?
+      "已付款"
+    else
+      "尚未付款"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :admin do  # admin底下有 products跟users兩個restful架構 , users底下的 to_admin to_normal action是post動作 (?)
     resources :products
-    resources :orders
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end
     resources :users do
       post :to_admin
       post :to_normal
