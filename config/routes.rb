75ac30e4fccd038381c88ebcb00a12512b307cf4
wesleychu <jrubyuncle@gmail.com>
2015-11-19 15:37:06 +0800

    建立 admin/orders 可以看到系統內所有訂單 ref: https://github.com/sdlong/artstore/commit/079fa353d28ea7a5f2dd0314762b6f249fdfd759

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
   namespace :admin do
     resources :products
     resources :users do
       post :to_admin
       post :to_normal
     end
+    resources :orders
   end
 
   resources :products do
