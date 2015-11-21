3b6cbc2d4659e7c5c58bc5d12c13f5e6c7955807
AlvinChiu <alvinhsian@gmail.com>
2015-11-21 11:45:47 +0800

    購物車加入「數量」設計 ( 數量為 0 的貨物不能「購買」)

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -14,8 +14,12 @@ class CartItemsController < ApplicationController
   def update
     @cart = current_cart
     @item = @cart.cart_items.find_by(product_id: params[:id])
-
-    @item.update(item_params)
+    if @item.product.quantity >= item_params[:quantity].to_i
+      @item.update(item_params)
+      flash[:notice] = "成功變更數量"
+    else
+      flash[:warning] = "數量不足，不能加入購物車"
+    end
 
     redirect_to carts_path
   end
