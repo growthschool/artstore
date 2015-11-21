5cca91e4a27f0dd20b6b6686db727e8dc19fb0eb
AlvinChiu <alvinhsian@gmail.com>
2015-11-21 12:17:53 +0800

    定義 find_cart_item 來簡化常用的程式

--- app/models/order.rb
+++ app/models/order.rb
@@ -12,7 +12,7 @@ class Order < ActiveRecord::Base
     cart.items.each do |cart_item|
       item = items.build
       item.product_name = cart_item.title
-      item.quantity = cart.cart_items.find_by(product_id: cart_item).quantity
+      item.quantity = cart.find_cart_item(cart_item).quantity
       item.price = cart_item.price
       item.save
     end
