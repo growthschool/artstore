6eb047d67e7997560ca3b5aee6b0670c776a1198
wesleychu <jrubyuncle@gmail.com>
2015-11-25 11:32:52 +0800

    implement add_to_cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,29 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find params[:id]
   end
+
+  def add_to_cart
+    cid = session[:cart_id]
+    if cid.nil?
+      c = Cart.create
+      session[:cart_id] = c.id
+    else
+      c = Cart.find_by(id: cid)
+      if c.nil?
+        c = Cart.new
+        session[:cart_id] = c.id
+      end
+    end
+
+    @product = Product.find(params[:id])
+
+    if c.items.include?(@product)
+      flash[:warning] = "already have"
+    else
+      c.items << @product
+      flash[:notice] = "add ok"
+    end
+
+    redirect_to :back
+  end
 end
