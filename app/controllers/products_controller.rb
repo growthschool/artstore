3a75428c0ddc76a62063cdabaf5aafcba46b39ab
wesleychu <jrubyuncle@gmail.com>
2015-11-25 16:46:22 +0800

    fix typo

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,7 +14,7 @@ class ProductsController < ApplicationController
       flash[:warning] = "你的購物車內已有此物品"
     else
       current_cart.add_product_to_cart(@product)
-      flash[:notice] = "你已成功將 #{product.title} 加入購物車"
+      flash[:notice] = "你已成功將 #{@product.title} 加入購物車"
     end
 
     redirect_to :back
