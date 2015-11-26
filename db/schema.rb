f8c54ffcd93e01bd42748e1a6b4b9f73969eb274
wesleychu <jrubyuncle@gmail.com>
2015-11-26 15:32:23 +0800

    add aasm_state field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151126062859) do
+ActiveRecord::Schema.define(version: 20151126072520) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -47,11 +47,12 @@ ActiveRecord::Schema.define(version: 20151126062859) do
   create_table "orders", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "total",          default: 0
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
     t.string   "token"
     t.boolean  "is_paid",        default: false
     t.string   "payment_method"
+    t.string   "aasm_state",     default: "order_placed"
   end
 
   add_index "orders", ["token"], name: "index_orders_on_token"
