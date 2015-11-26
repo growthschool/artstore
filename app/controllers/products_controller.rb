baad6e55a64c3feac097119bc5180520166ed173
zhangkon <zhangkon@gmail.com>
2015-11-26 16:14:16 +0800

    class week2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,16 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    if !current_cart.items.include?(@product)
+      current_cart.add_product_to_cart(@product)
+      flash[:notice]= "您已成功加入商品！"
+    else
+      flash[:alert]= "商品已經在購物車中了！"
+    end
+
+    redirect_to :back
+  end
 end
