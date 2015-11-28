438453179fc6dc56f34e6e40c5bbd6bc3d2de83e
wesleychu <jrubyuncle@gmail.com>
2015-11-28 18:11:22 +0800

    add more message

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -2,4 +2,14 @@ module OrdersHelper
   def render_order_state(order)
     t("orders.order_state.#{order.aasm_state}")
   end
+
+  def render_order_paid_state(order)
+    if order.paid?
+      "已付款"
+    else
+      order_state = render_order_state(order)
+      "未付款 - #{order_state}"
+    end
+
+  end
 end
