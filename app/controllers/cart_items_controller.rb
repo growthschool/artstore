bfbda71543fd6d47ac7cd048adca156bb8deb3f8
wesleychu <jrubyuncle@gmail.com>
2015-11-28 15:29:54 +0800

    in carts#index, allow user to modify product quantity

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,6 +1,18 @@
 class CartItemsController < ApplicationController
   before_action :authenticate_user!
 
+  def update
+    #params[:id] is product id
+    item = current_cart.cart_items.find_by(product_id: params[:id])
+
+    item.update(cart_item_params)
+
+    #item.quantity = cart_item_params[:quantity]
+    #item.save
+
+    redirect_to :back
+  end
+
   def destroy
     #params[:id] is product id
     item = current_cart.cart_items.find_by(product_id: params[:id])
@@ -9,5 +21,10 @@ class CartItemsController < ApplicationController
 
     flash[:warning] = "成功將 #{product_title} 從購物車刪除!"
     redirect_to :back
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity)
   end
 end
