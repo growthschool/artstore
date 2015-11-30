e9a71aab286a716b3bb9b5e5badbfb0057614503
zhangkon <zhangkon@gmail.com>
2015-11-30 12:26:52 +0800

    total price

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,6 +1,15 @@
 class CartItemsController < ApplicationController
   before_action :authenticate_user!
 
+  def update
+    @cart = current_cart
+    @item = @cart.cart_items.find_by(product_id: params[:id])
+
+    @item.update(item_params)
+    redirect_to carts_path
+
+  end
+
   def destroy
     @cart = current_cart
     @item = @cart.cart_items.find_by(product_id: params[:id])
@@ -9,5 +18,11 @@ class CartItemsController < ApplicationController
 
     flash[:warning] = "您已經將 #{@product.title}從購物車中刪除！"
     redirect_to :back
+  end
+
+  private
+
+  def item_params
+    params.require(:cart_item).permit(:quantity)
   end
 end
