6044760464c5b3ce8494af62cf4416c93d1d55c6
AndyWang1116 <andywang1116@gmail.com>
2015-11-30 12:19:06 +0800

    none

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  # rememerable 實作remember_me
+  # rememberable 實作remember_me
   #
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   has_many :orders
 
 # 我要db吃 "is_admin" 這個欄位 ,須使用 rails g migration add_is_admin_to_user
-# 並且到db裡檔案修改 :users, :is_admin, :boolean, defaut: false  # default如果是true那大家都是admin了!
+# 並且到db裡檔案修改 :users, :is_admin, :boolean, default: false  # default如果是true那大家都是admin了!
 # 因為default: false 無法直接於指令內添加，所以只能另外到db檔案內修改了
   def admin?
     is_admin
