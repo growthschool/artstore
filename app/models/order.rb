71437ba87bcd9be20650e98e4d5b5d08f530f3b1
kakas <shrimptrain@gmail.com>
2015-12-01 21:56:44 +0800

    將訂單結帳

--- app/models/order.rb
+++ app/models/order.rb
@@ -26,4 +26,12 @@ class Order < ActiveRecord::Base
     self.total = cart.total_price
     self.save
   end
+
+  def set_payment_with!(method)
+    self.update_columns(payment_method: method)
+  end
+
+  def pay!
+    self.update_columns(is_paid: true)
+  end
 end
