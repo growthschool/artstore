a29e34701599f9befcda8e2d2cabc944d0d0144e
wesleychu <jrubyuncle@gmail.com>
2015-12-01 20:28:18 +0800

    sync

--- config/initializers/pay2go.rb
+++ config/initializers/pay2go.rb
@@ -1,15 +1,20 @@
 Pay2go.setup do |pay2go|
   if Rails.env.development?
-    pay2go.merchant_id = "11099989"  # 放測試站的 key
+    pay2go.merchant_id = ENV['pay2go_merchant_id']
+    pay2go.hash_key    = ENV['pay2go_hash_key']
+    pay2go.hash_iv     = ENV['pay2go_hash_iv']
 
-    pay2go.hash_key    = "aDau45heoxZrYbUHv47dm7oNUSB2A7TL"
-    pay2go.hash_iv     = "K3fP8x4x53Z3fTLK"
     pay2go.service_url = "https://capi.pay2go.com/MPG/mpg_gateway"
   else
-    pay2go.merchant_id = "1234567"  # 放正式站的 key
+    pay2go.merchant_id = "11099989"  # 放正式站的 key
+    pay2go.hash_key    = "ZEobJ33C9ExBptxhEXufl7pmjmfI5MY4"
+    pay2go.hash_iv     = "qngaqjMzXkbnOIEt"
 
-    pay2go.hash_key    = "xxxxxxxx"
-    pay2go.hash_iv     = "xxxxxxxx"
-    pay2go.service_url = "https://api.pay2go.com/MPG/mpg_gateway"
+    #pay2go.merchant_id = ENV['pay2go_merchant_id']
+    #pay2go.hash_key    = ENV['pay2go_hash_key']
+    #pay2go.hash_iv     = ENV['pay2go_hash_iv']
+
+    pay2go.service_url = "https://capi.pay2go.com/MPG/mpg_gateway"
+    #pay2go.service_url = "https://api.pay2go.com/MPG/mpg_gateway"
   end
 end
