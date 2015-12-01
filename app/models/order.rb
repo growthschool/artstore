8d1731777809bfc506ea8c6b040766c974c99c74
Wei-Cheng Yang <rainyday309@gmail.com>
2015-12-01 16:06:41 +0800

    建立訂單（顯示訂單內容）

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,12 @@ class Order < ActiveRecord::Base
 	has_one :info, class_name: "OrderInfo", dependent: :destroy 
 
 	accepts_nested_attributes_for :info
+	before_create :generate_token
+
+	def generate_token
+		self.token = SecureRandom.uuid
+		
+	end
 
 	def build_item_cache_from_cart(cart)
 		cart.items.each do |cart_item|
