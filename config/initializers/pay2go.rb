97ad49bec304e156c28820cc4eda62220dcc8ce5
wesleychu <jrubyuncle@gmail.com>
2015-12-01 21:28:36 +0800

    use figaro

--- config/initializers/pay2go.rb
+++ config/initializers/pay2go.rb
@@ -6,13 +6,13 @@ Pay2go.setup do |pay2go|
 
     pay2go.service_url = "https://capi.pay2go.com/MPG/mpg_gateway"
   else
-    pay2go.merchant_id = "11099989"  # 放正式站的 key
-    pay2go.hash_key    = "ZEobJ33C9ExBptxhEXufl7pmjmfI5MY4"
-    pay2go.hash_iv     = "qngaqjMzXkbnOIEt"
+    #pay2go.merchant_id = "11099989"  # 放正式站的 key
+    #pay2go.hash_key    = "ZEobJ33C9ExBptxhEXufl7pmjmfI5MY4"
+    #pay2go.hash_iv     = "qngaqjMzXkbnOIEt"
 
-    #pay2go.merchant_id = ENV['pay2go_merchant_id']
-    #pay2go.hash_key    = ENV['pay2go_hash_key']
-    #pay2go.hash_iv     = ENV['pay2go_hash_iv']
+    pay2go.merchant_id = ENV['pay2go_merchant_id']
+    pay2go.hash_key    = ENV['pay2go_hash_key']
+    pay2go.hash_iv     = ENV['pay2go_hash_iv']
 
     pay2go.service_url = "https://capi.pay2go.com/MPG/mpg_gateway"
     #pay2go.service_url = "https://api.pay2go.com/MPG/mpg_gateway"
