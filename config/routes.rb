2e358bd33070d43cf0761a0209dd51e77fe58a1b
kakas <shrimptrain@gmail.com>
2015-12-01 21:45:02 +0800

    建立訂單

--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,16 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :orders
+
   root "products#index"
 
-  resources :carts
+  resources :carts do
+    collection do
+      post :checkout
+    end
+  end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
