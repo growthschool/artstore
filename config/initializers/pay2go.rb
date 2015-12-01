a66f55e1748f373cebf810527a1e25592a007822
AndyWang1116 <andywang1116@gmail.com>
2015-12-01 21:54:54 +0800

    S3申請完,本機端圖片上傳測試成功,token納入figaro

--- config/initializers/pay2go.rb
+++ config/initializers/pay2go.rb
@@ -1,14 +1,14 @@
 Pay2go.setup do |pay2go|
   if Rails.env.development?
-    pay2go.merchant_id = "11098372"
-    pay2go.hash_key    = "M0KoE54RVlMB28RSD0rkq2r3ce9bPO4B"
-    pay2go.hash_iv     = "PP0uV4lRq8tsgklT"
-    pay2go.service_url = "https://capi.pay2go.com/MPG/mpg_gateway"
+    pay2go.merchant_id = ENV["pay2go.merchant_id"]
+    pay2go.hash_key    = ENV["pay2go.hash_key"]
+    pay2go.hash_iv     = ENV["pay2go.hash_iv"]
+    pay2go.service_url = ENV["pay2go.service_url"]
   else
-    pay2go.merchant_id = "11098372  "
-    pay2go.hash_key    = "M0KoE54RVlMB28RSD0rkq2r3ce9bPO4B"
-    pay2go.hash_iv     = "PP0uV4lRq8tsgklT"
-    pay2go.service_url = "https://api.pay2go.com.MPG/mpg_gateway"
+    pay2go.merchant_id = ENV["pay2go.merchant_id"]
+    pay2go.hash_key    = ENV["pay2go.hash_key"]
+    pay2go.hash_iv     = ENV["pay2go.hash_iv"]
+    pay2go.service_url = ENV["pay2go.service_url"]
   end
 
 end
