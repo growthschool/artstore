f3c449250cfb557696c21f4927abfef7913ac8e6
wesleychu <jrubyuncle@gmail.com>
2015-12-01 16:36:07 +0800

    fix bug and enable e-mail

--- app/services/order_placing_service.rb
+++ app/services/order_placing_service.rb
@@ -6,9 +6,9 @@ class OrderPlacingService
 
   def place_order!
     @order.build_item_cache_from_cart(@cart)
-    @order.calculate_total!(@cart)
+    @order.calculate_total(@cart)
     @cart.clean!
-    # OrderMailer.notify_order_placed(@order).deliver
+    OrderMailer.notify_order_placed(@order).deliver
 
   end
 end
