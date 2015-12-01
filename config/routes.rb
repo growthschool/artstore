2e1e77b7815ef359bfadc347cce8b53607e51efa
Wei-Cheng Yang <rainyday309@gmail.com>
2015-12-02 07:48:27 +0800

    將訂單結帳

--- config/routes.rb
+++ config/routes.rb
@@ -45,7 +45,11 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :orders
+  resources :orders do
+    member do
+      get :pay_with_credit_card
+    end
+  end
 
   # Example resource route with sub-resources:
   #   resources :products do
