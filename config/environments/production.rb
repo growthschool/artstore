e353784481f7c4e94bfa36c250a65703127e1e83
wesleychu <jrubyuncle@gmail.com>
2015-12-01 20:31:42 +0800

    set Pay2go.integration_mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.after_initialize do
-    Pay2go.integration_mode = :production
+    Pay2go.integration_mode = :development #:production
   end
 end
