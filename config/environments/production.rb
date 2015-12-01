5585e9ad21a3ec5580d9664d4b98a8e3f91146b3
wesleychu <jrubyuncle@gmail.com>
2015-12-01 18:53:56 +0800

    set config.action_mailer.default_url_options on product

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.smtp_settings = {
     port:           587,
     address:        "smtp.mailgun.org",
