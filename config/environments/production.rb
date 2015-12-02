8f2032aaddb8e97a39f05be4e4358b6e501b2d0f
Wei-Cheng Yang <rainyday309@gmail.com>
2015-12-02 20:07:42 +0800

    fix minor bug for heroku deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,9 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.after_initialize do
-    Pay2go.integration_mode = :production
+    Pay2go.integration_mode = :development
   end
+
+  config.action_mailer.default_url_options = {host: 'https://thawing-tundra-4435.herokuapp.com'}
+
 end
