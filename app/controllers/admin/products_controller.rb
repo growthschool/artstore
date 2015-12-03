fd6be3a64c5d6a111354438a9c66663bb7f1057f
Shawn <management2e@gmail.com>
2015-12-03 13:33:27 +0800

    必須要先登入才能進入

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,8 +1,11 @@
 class Admin::ProductsController < ApplicationController
+
+  before_action :authenticate_user!
+
   def index
     @products = Product.all
   end
-  
+
   def new
     @product = Product.new
   end
