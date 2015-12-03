cf7990029eca932b9a83a4f2289b499085c14378
wesleychu <jrubyuncle@gmail.com>
2015-12-03 13:43:08 +0800

    add setting

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,14 +3,14 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     'xxx',      # 你的 key
+      aws_access_key_id:     ENV['aws_access_key_id'],     # 你的 key
 
-      aws_secret_access_key: 'yyy',      # 你的 secret key
+      aws_secret_access_key: ENV['aws_secret_access_key'], # 你的 secret key
 
-      region:                'eu-west-1' # 你的 S3 bucket 的 Region 位置
+      region:                'us-west-1' # 你的 S3 bucket 的 Region 位置
 
     }
-    config.fog_directory  = 'xxxx' # 你設定的 bucket name
+    config.fog_directory  = 'bucketcat' # 你設定的 bucket name
 
   else
     config.storage :file
