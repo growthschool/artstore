269855be61d22f091ad0f76d4b20c0cd45b1cba8
AlvinChiu <alvinhsian@gmail.com>
2015-12-05 10:41:42 +0800

    fix AddIsAdminToUser boolean

--- db/migrate/20151111130037_add_is_admin_to_user.rb
+++ db/migrate/20151111130037_add_is_admin_to_user.rb
@@ -1,5 +1,5 @@
 class AddIsAdminToUser < ActiveRecord::Migration
   def change
-    add_column :users, :is_admin, :bloolean, default: false
+    add_column :users, :is_admin, :boolean, default: false
   end
 end
