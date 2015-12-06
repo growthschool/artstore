d3e63294d34724d7048f500a5a6ea2c9a323e416
AlvinChiu <alvinhsian@gmail.com>
2015-12-06 11:12:56 +0800

    1. 解決在 heroku 環境下無法完成刷卡流程的問題 2. 將 pay2go 的環境改為開發用

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = {host: 'aqueous-refuge-9869.herokuapp.com'}
   config.after_initialize do
-    Pay2go.integration_mode = :production
+    # Pay2go.integration_mode = :production
+    Pay2go.integration_mode = :development
   end
 end
