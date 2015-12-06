57e57379541997ec82f557a7c877a4a3e121511b
haneric <haneric1986@gmail.com>
2015-12-06 21:57:25 +0800

    訂單編號加上時戳

--- app/services/pay2go_service.rb
+++ app/services/pay2go_service.rb
@@ -3,7 +3,7 @@ class Pay2goService
   def initialize(order)
     @order = order
     @timestamp = order.created_at.to_i
-    @merchant_order_no = order.id
+    @merchant_order_no = "#{order.id}s#{Time.now.strftime("%Y%m%d%H%M%S")}"
     @total_price = order.total
   end
 
