35c0573224fe075bcc7998565bc37da34da6eb1e
wesleychu <jrubyuncle@gmail.com>
2015-12-07 15:31:57 +0800

    change MerchantOrderNo

--- app/services/pay2go_service.rb
+++ app/services/pay2go_service.rb
@@ -3,7 +3,8 @@ class Pay2goService
   def initialize(order)
     @order = order
     @timestamp = order.created_at.to_i
-    @merchant_order_no = "#{order.id}s32"
+    #@merchant_order_no = "#{order.id}s32"
+    @merchant_order_no = gen_order_no
     @total_price = order.total
   end
 
@@ -15,4 +16,8 @@ class Pay2goService
     "HashKey=#{Pay2go.hash_key}&Amt=#{@total_price}&MerchantID=#{Pay2go.merchant_id}&MerchantOrderNo=#{@merchant_order_no}&TimeStamp=#{@timestamp}&Version=1.1&HashIV=#{Pay2go.hash_iv}"
   end
 
+  def gen_order_no
+    "#{@order.id}_#{Date.today.to_time.to_i}"
+    #{}"#{@order.id}s32"
+  end
 end
