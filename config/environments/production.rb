a548e39d63c2d3923f40545be1bdffee407b2c72
Alex Liagn <u9211011@gmail.com>
2015-12-08 23:17:31 +0800

    Using development setting in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.after_initialize do
-    Pay2go.integration_mode = :production
+    Pay2go.integration_mode = :development
   end
 
   config.action_mailer.default_url_options = { host: 'https://morning-falls-3339.herokuapp.com' }
