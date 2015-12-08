ef3f6c0e5ec31cbecb8750fdcae8f6f89da2015f
shawn <management2e@gmail.com>
2015-12-08 20:51:24 +0800

    設定  admin/products 的 routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :products
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
