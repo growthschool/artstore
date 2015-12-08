fd6d951ea2351dbb3eadd9b51df58147f5fe4ded
HenryWu <henrywu68@gmail.com>
2015-12-08 11:18:41 +0800

    ignore AWS keys and id

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,11 +3,11 @@ CarrierWave.configure do |config|
     config.storage :fog                       
     config.fog_credentials = {
       provider:              'AWS',                        
-      aws_access_key_id:     'AKIAIPN4WNEPYR5VDCNQ',      # 你的 key           
+      aws_access_key_id:     ENV['aws_access_key_id'],      # 你的 key           
 
-      aws_secret_access_key: 'SlwkRruxLkPJI46O4alGMUywtaXMKxt+K+YO7bY4',      # 你的 secret key         
+      aws_secret_access_key: ENV['aws_secret_access_key'],      # 你的 secret key         
 
-      region:                'us-east-1' # 你的 S3 bucket 的 Region 位置   
+      region:                ENV['region'] # 你的 S3 bucket 的 Region 位置   
 
     }
     config.fog_directory  = 'rails101' # 你設定的 bucket name 
