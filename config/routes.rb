348cb788280653b02a3d2288bef2404347bbafe8
shawn <management2e@gmail.com>
2015-12-09 14:44:15 +0800

    建立 前往訂單資訊頁的連結

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,11 @@ Rails.application.routes.draw do
 
   root "products#index"
 
-  resources :carts
+  resources :carts do
+    collection do
+      post :checkout
+    end
+  end
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
