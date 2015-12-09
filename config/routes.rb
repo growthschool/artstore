caac5646c839b4cefe56a0092d24ada46c84500e
shawn <management2e@gmail.com>
2015-12-09 17:26:02 +0800

    建立 admin/orders 可以看到系統內所有訂單

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
   namespace :admin do
     resources :products
+    resources :orders
+    
     resources :users do
       member do
         post :to_admin
