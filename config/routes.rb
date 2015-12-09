e3a53de21dd110b3782f9d3532ab3319c9c6dbf3
shawn <management2e@gmail.com>
2015-12-09 17:20:28 +0800

    完成 account/orders index

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,10 @@ Rails.application.routes.draw do
   end
 
   resources :items, controller: "cart_items"
+
+  namespace :account do
+    resources :orders
+  end
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
