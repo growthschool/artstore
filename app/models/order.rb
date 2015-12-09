58efe3363ed4f6230d0214a8d19bc6a1b8e8ddcc
shawn <management2e@gmail.com>
2015-12-09 15:33:01 +0800

    設定 order_controller  create_action 中用到的 method

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,19 @@ class Order < ActiveRecord::Base
   has_one :info, class_name: "OrderInfo", dependent: :destroy
 
   accepts_nested_attributes_for :info
+
+  def build_item_cache_from_cart(cart)
+    cart.items.each do |cart_item|
+      item = item.build
+      item.product_name = cart_item.title
+      item.quantity = 1
+      item.price = cart_item.price
+      item.save
+    end
+  end
+
+  def calculate_total!(cart)
+    self.total = cart.total_price
+    self.save
+  end
 end
