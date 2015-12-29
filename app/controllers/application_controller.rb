f212ac76f963e1e0a7acfd60070d3c1849b60875
niclin <bboyceo@hotmail.com>
2015-12-30 01:51:09 +0800

    新增帳戶設定功能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
  def admin_required
    if !current_user.admin?
@@ -27,4 +28,11 @@ class ApplicationController < ActionController::Base
    session[:cart_id] = cart.id
    cart
  end
+
+ protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
+  end
 end
