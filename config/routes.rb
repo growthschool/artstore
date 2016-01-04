548373f56fbc1ff31f9d40753bdb89752fab81b4
niclin <bboyceo@hotmail.com>
2016-01-04 16:44:33 +0800

    Add page

--- config/routes.rb
+++ config/routes.rb
@@ -45,6 +45,7 @@ Rails.application.routes.draw do
 
   root "products#index"
 
+  get "/pages/:action" , :controller => "pages"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
