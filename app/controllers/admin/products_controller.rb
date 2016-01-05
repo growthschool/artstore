4b388249a6b9aadf53cab4b6b03baba8bc46de50
PoYuan Huang <reticentminds@gmail.com>
2016-01-05 16:07:16 +0800

    作業前

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,41 @@
+class Admin::ProductsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :admin_required
+	
+	def new
+		@product = Product.new
+	end
+	def create
+		@product = Product.new(product_params)
+		if @product.save
+			redirect_to admin_products_path
+		else
+			render :new
+		end
+	end
+	
+	def index
+		@products = Product.all
+	end
+
+	def show
+		@product = Product.find[params[:id]]
+	end
+
+	def edit
+		@product = Product.find[params[:id]]
+	end
+	
+	def update
+	end
+
+	def destroy
+		@product = Product.find[params[:id]]
+	end
+
+private
+	def product_params
+		params.require(:product).permit(:title, :description, :quantity, :price)
+	end
+
+end
