314638dc088b89c86cfe30a584b6210dd3360e8a
jimmypan <jimmypan@gmail.com>
2016-01-05 22:54:15 +0800

    Frontend products#index and products#show ready

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160104131119) do
+ActiveRecord::Schema.define(version: 20160105123710) do
+
+  create_table "photos", force: :cascade do |t|
+    t.integer  "product_id"
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
