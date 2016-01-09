ec2d3fc9dddae0afc4e96c4f978a25613884031d
chieh <chiehandlu@icloud.com>
2016-01-09 10:05:39 +0800

    homework finish-1

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,60 @@
+class Admin::ProductsController < ApplicationController
+
+  layout "admin"
+
+  before_action :authenticate_user!
+  before_action :admin_required
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+    if @product.photo.present?
+       @photo = @product.photo
+     else
+       @photo = @product.build_photo
+     end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+    @photo = @product.photo
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if@product = Product.find(params[:id])
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to admin_products_path
+    else
+      render :new
+    end
+  end
+  def update
+    @product = Product.update(product_params)
+
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def product_params
+    params.required(:product).permit(:title, :description, :quantity, :price, photo_attributes: [:image, :id])
+  end
+
+end
