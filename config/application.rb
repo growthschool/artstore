e95bcfcd38e53adf3e0d1c8d0b3972cdc2dc9ba1
mariahsiao <thatsjustmeiwant@gmail.com>
2016-01-10 20:52:08 +0800

    Fix bug

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Artstore
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    config.autoload_paths += %W(#{config.root}/app/uploaders)
+
   end
 end
