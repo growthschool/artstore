dff3e7092fe2803bc5d8b978e0f9f55943cbb9bf
Stan <solve153@gmail.com>
2016-01-11 20:04:14 +0800

    購物車的商品明細

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
 
   root "products#index"
   
+  resources :carts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
