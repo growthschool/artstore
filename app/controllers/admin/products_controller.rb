deafab39421486734456d90905fb44f50a039c24
Chia-Yu Lin <sallylin0121@gmail.com>
2016-01-11 17:13:45 +0800

    install imagemagick & front end new/edit page upload photos & show index page

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -9,10 +9,16 @@ class Admin::ProductsController < ApplicationController
 
   def new
     @product = Product.new
+    @photo = @product.build_photo
   end
 
   def edit
     @product = Product.find(params[:id])
+    if @product.photo.present?
+       @photo = @product.photo
+    else
+       @photo = @product.build_photo
+    end
   end
 
   def update
@@ -46,7 +52,7 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price)
+    params.require(:product).permit(:title, :description, :quantity, :price,photo_attributes: [:image, :id])
   end
 
 end
