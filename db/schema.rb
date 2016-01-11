3e66164d637eee00af2dab0dc0de7ff61347757d
Chia-Yu Lin <sallylin0121@gmail.com>
2016-01-11 11:00:46 +0800

    add check admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160111024717) do
+ActiveRecord::Schema.define(version: 20160111025943) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
