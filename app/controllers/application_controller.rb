54d7ce20e8b59000d72ff658418dd881d88a3ba0
PoYuan Huang <reticentminds@gmail.com>
2016-01-11 22:36:41 +0800

    第二週上課 (幾乎都用貼的QQ)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,24 @@ class ApplicationController < ActionController::Base
   	if !current_user.admin?
   		redirect_to "/"
   	end
-  end
+	end
+
+	helper_method :current_cart
+
+	def current_cart
+		@current_cart ||= find_cart	
+		end
+
+		private
+
+		def find_cart
+		cart = Cart.find_by(id: session[:cart_id])
+
+		unless cart.present?
+			cart = Cart.create
+		end
+
+		session[:cart_id] = cart.id
+		cart
+	end
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -12,6 +12,8 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
+  
+  #儲存位置 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
