4c118f8cacf925142e70df147221e2eec78cef46
jar951 <jar9518@gmail.com>
2016-01-25 20:46:31 +0800

    Gem install for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+u = User.new
+u.email = "jar9518@gmail.com"
+u.password = "j0911218189"
+u.password_confirmation = "j0911218189"
+u.is_admin = true
+u.save
