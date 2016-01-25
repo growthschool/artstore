388f634b885db0f3866f82c8d3e98ab828c436ad
jimmypan <jimmypan@gmail.com>
2016-01-25 18:35:16 +0800

    correctd the file path of devise.zh-TW.yml and zh-TW.yml

--- app/controllers/account/orders_controller.rb
+++ app/controllers/account/orders_controller.rb
@@ -4,4 +4,6 @@ class Account::OrdersController < ApplicationController
 	def index
 		@orders = current_user.orders.order("id DESC")
 	end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,14 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
   namespace :admin do
     resources :products 
-    resources :orders
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end
     resources :users do 
       member do
         post :to_admin
