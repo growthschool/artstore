7d55d3ef79f986140bbfa339fc4f5c955d2820ff
Poligen Chen <scipio@poligenc.info>
2016-01-25 17:07:02 +0800

    finish 1-3 only admin can login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125085317) do
+ActiveRecord::Schema.define(version: 20160125090319) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
