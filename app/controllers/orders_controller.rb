e343390c1d071bc6a2fb6835826e195afb1c376e
Poligen Chen <scipio@poligenc.info>
2016-01-27 16:13:43 +0800

    建立訂單(生成)

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,24 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  def create
+    @order = current_user.orders.build(order_params)
+
+    if @order.save
+      @order.build_item_cache_from_cart(current_cart)
+      @order.calculate_total!(current_cart)
+      redirect_to order_path(@order)
+    else
+      render "carts/checkout"
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(info_attributes: [:billing_name, 
+                                 :billing_address, 
+                                 :shipping_name,
+                                 :shipping_address])
+
+  end
+end
