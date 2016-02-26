fcd7cb40477461130b388be10d421b0eabb06880
sdlong <sdlong.jeng@gmail.com>
2016-02-26 11:39:40 +0800

    完成轉換使用者權限功能

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   root "products#index"
+
   devise_for :users
+
   namespace :admin do
     resources :products
-    resources :users
+    resources :users do
+      member do
+        post :to_admin
+        post :to_normal
+      end
+    end
   end
 
   resources :products
