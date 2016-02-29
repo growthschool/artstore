a42a9391b960525ca2365c537ce454e6066ae024
Jason Lung <lionlies@gmail.com>
2016-02-29 21:56:01 +0800

    changed seeds.rb content

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 u = User.new
-u.email = "lionlies@gmail.com"
+u.email = "admin@test.com"
 u.password = "12345678"
 u.password_confirmation = "12345678"
 u.is_admin = true
