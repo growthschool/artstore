44f7c74c85d0a5a75c6651df5e2d1a1463dcf3ac
mindy <mindy22050@gmail.com>
2016-02-29 21:38:32 +0800

    deploy to heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
-class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
+class ApplicationController < ActionController::Base # “<”是指繼承
+    # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+    def admin_required
+      if !current_user.admin? #如果這個人不是admin
+        redirect_to "/"  #把它導回首頁
+      end
+    end
 end
