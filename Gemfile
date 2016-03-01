bb838c7427528e184d0a9bf2d69cc538ab6183d3
Tsung-Ta Wu <tsungta.wu@gmail.com>
2016-03-01 21:42:12 +0800

    To W1 HK

--- Gemfile
+++ Gemfile
@@ -28,7 +28,9 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem "devise"
 gem "bootstrap-sass"
-
+gem "simple_form"
+gem "carrierwave"
+gem "mini_magick"
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
@@ -48,4 +50,3 @@ group :development, :test do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,11 +1,35 @@
 class Admin::ProductsController < ApplicationController
 
+layout "admin"
+
 before_action :authenticate_user!
 before_action :admin_required
 
-	def new 
+	def new
 		@product = Product.new
+		@photo =@product.build_photo
 	end
+
+	def edit
+		@product = Product.find(params[:id])
+
+		if @product.photo.present?
+			@photo =@product.photo
+		else
+			@photo =@product.build_photo
+		end
+	end
+
+	def update
+		@product = Product.find(params[:id])
+
+		if @product.update(product_params)
+			redirect_to admin_products_path
+		else
+			render :edit
+		end
+	end
+
 
 	def create
 		@product =Product.new(product_params)
@@ -25,7 +49,7 @@ before_action :admin_required
 	private
 
 	def product_params
-		params.require(:product).permit(:title, :description, :quantity, :price)
+		params.require(:product).permit(:title, :description, :quantity, :price, photo_attributes: [:image, :id])
 	end
 
 
