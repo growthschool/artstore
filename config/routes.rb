448d20edb1a90b0f53891ffb46abe2fb8e6b0c7d
rjjq <rrrjjjqqq@gmail.com>
2016-03-02 21:43:40 +0800

    第一週上課

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root "welcome#index"
 
+  devise_for :users
+
+  namespace :admin do
+    resources :products
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
