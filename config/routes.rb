cf6d87c32a4c96f74847c647c7833eb355e4622a
yhheho <yhheho@gmail.com.tw>
2016-03-02 21:49:58 +0800

    Deploy heroku again

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :products
   end
 
-  #root "welcome#index"
+  root "welcome#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
