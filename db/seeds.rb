d34dcf4a83391a21941ada7c143386dd785ea3d4
OUYANGYI <ouyangyi0831@gmail.com>
2016-03-02 21:42:46 +0800

    deploy to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+u = User.new
+u.email = "ouyangyi0831@gmail.com"           # 可以改成自己的 email
+
+u.password = "0uyangy1"              # 最少要八碼
+
+u.password_confirmation = "0uyangy1" # 最少要八碼
+
+u.is_admin = true
+u.save
