4cf3459746529b1812f9cbc34efcbdae790cbf5d
本名 <futp3249@hotmail.com>
2016-03-03 16:26:19 +0800

    權限管理

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
 root "products#index"
   devise_for :users
-  
+
   namespace :admin do
    resources :products
+   resources :users do
+     member do
+        post :to_admin
+        post :to_normal
+     end
+    end
   end
 
 
