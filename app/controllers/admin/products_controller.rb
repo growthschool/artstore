ffeb82541dd417a11ff9cb2e64cd0c6bf54a47ac
yihung <wendy0821@gmail.com>
2016-03-03 12:24:53 +0800

    deploy to heroku

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,14 +1,37 @@
 class Admin::ProductsController < ApplicationController
 
+  layout "admin"
+
   before_action :authenticate_user!
   before_action :admin_required
 
-    def index
+  def index
       @products = Product.all
     end
 
   def new
     @product = Product.new
+    @photo = @product.build_photo
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+
+    if @product.photo.present?
+      @photo = @product.photo
+    else
+      @photo = @product.build_photo
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
   end
 
   def create
@@ -21,9 +44,16 @@ class Admin::ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to admin_products_path
+  end
+
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price)
+   params.require(:product).permit(:title, :description, :quantity, :price,
+                                    photo_attributes: [:image, :id])
   end
 end
