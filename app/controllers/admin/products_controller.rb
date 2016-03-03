55ad714ad9f7796caecd8bf98bcb9b5f20e37b26
Daniel Lin <zarr12steven@hotmail.com>
2016-03-03 18:48:43 +0800

    前台實作 & 建立後台layout

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,10 +1,12 @@
 class Admin::ProductsController < ApplicationController
+
+  layout "admin"
+
   before_action :authenticate_user!
   before_action :admin_required
 
   def index
     @products = Product.all
-    @photo = @product.build_photo
   end
 
   def new
