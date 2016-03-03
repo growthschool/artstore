e253247683f1bd3ca58fe0f9e9ee9ef1aaad26fc
ponpon <sheep0406@hotmail.com>
2016-03-03 11:29:08 +0800

    Add admin edit stuff

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,27 +1,50 @@
 class Admin::ProductsController < ApplicationController
 
+  layout "admin"
   before_action :authenticate_user!
   before_action :admin_required
   def new 
     @product = Product.new
+    @photo = @product.build_photo
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+
+    if @product.photo.present?
+      @photo = @product.photo
+    else
+      @photo = @product.build_photo
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
   end
 
   def create
     @product = Product.new(product_params)
 
-    if @prodcut.save
+    if @product.save
       redirect_to admin_products_path
     else
       render :new
     end
   end
+
   def index
     @products = Product.all
   end
 
-  private
+  private 
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price)
+    params.require(:product).permit(:title, :description, :quantity, :price, photo_attributes: [:image, :id])
   end
 end
