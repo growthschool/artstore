62350d0610513ee0749f9a2e499a47f18c992530
JerryLu <icecaramelmilktea@yahoo.com.tw>
2016-03-04 18:17:33 +0800

    week1 homework step1

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
-  root "welcome#index"
+  root "products#index"
+  resources :products
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
