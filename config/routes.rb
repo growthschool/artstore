037d59eba48ac637cfc3f931224e021c193f46b9
Chad Yeh <chadyeh@MacBook-Pro-2.local>
2016-03-06 02:03:18 +0800

    新增Nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root "welcome#index"
+  root "products#index"
 
+  devise_for :users
   namespace :admin do
     resources :products
   end
+
+  resources :products
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
