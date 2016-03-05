c94e63b02bcbd476d462500aacd288cf17eceec9
ybyap <yb317@hotmail.com>
2016-03-05 17:30:20 +0800

    Add product index and show pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  root "welcome#index"
+  root "products#index"
+
   devise_for :users
   namespace :admin do
     resources :products
   end
+
+  resources :products
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
