769e6509ee40b1f359afd3507ce97af4d0b96b97
Chad Yeh <chadyeh@MacBook-Pro-2.local>
2016-03-06 02:09:53 +0800

    add products edit function

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,7 @@
 class Admin::ProductsController < ApplicationController
   before_action :authenticate_user!
   before_action :admin_required
-  
+
   def index
     @products = Product.all
   end
@@ -17,6 +17,20 @@ class Admin::ProductsController < ApplicationController
       redirect_to admin_products_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to admin_products_path
+    else
+      render :edit
     end
   end
 
