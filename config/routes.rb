a76bb2fe4a0b03526950f902bae63843524136ba
yoyyotang <yoyyotang@gmail.com>
2016-03-06 01:50:23 +0800

    store-v2-homework deploy to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "welcome#index"
+  root "products#index"
 
   namespace :admin do
     resources :products
+    resources :users do
+      member do
+        post :to_admin
+        post :to_normal
+      end
+    end
   end
+
+  resources :products
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
