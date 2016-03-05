73920ed08facd6c0fa9dcea38c164108b3abdf1b
langoct25 <langoct25@gmail.com>
2016-03-06 04:28:54 +0800

    課堂進度完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   #view and edit 2016.3.1 18:02 by LaurentLang
+  def admin_required
+    if !current_user.admin?
+      redirect_to "http://tw.yahoo.com"
+    end
+  end
 end
