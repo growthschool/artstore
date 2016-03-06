482d422600aec748d5277055c158f5d0ac605d5d
amytseng <aammyytseng@gmail.com>
2016-03-06 23:46:42 +0800

    deploy to heroku

--- Gemfile
+++ Gemfile
@@ -1,6 +1,13 @@
 source 'https://rubygems.org'
+gem "bootstrap-sass"
+gem "devise"
+gem "simple_form"
 
+gem "carrierwave"
+gem "mini_magick"
+gem 'sqlite3', group: :development
 
+gem "font-awesome-rails"
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.2'
 # Use sqlite3 as the database for Active Record
@@ -31,6 +38,11 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+
+  group :production do
+    gem "pg"
+    gem "rails_12factor"
+  end
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
-  root "welcome#index"
-
+  devise_for :users
+   namespace :admin do
+     resources :products
+     resources :users do
+       member do
+         post :to_admin
+         post :to_normal
+       end
+     end
+   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  
+  root "products#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -55,4 +64,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  resources :products
 end
