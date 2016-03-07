ea038bde0fb6e164ea11a55417c8ee9a7ace942e
Chad Yeh <chadyeh@MacBook-Pro-2.local>
2016-03-07 21:36:46 +0800

    顯示訂單內容

--- app/models/order.rb
+++ app/models/order.rb
@@ -20,4 +20,10 @@ class Order < ActiveRecord::Base
     self.total = cart.total_price
     self.save
   end
+
+  before_create :generate_token
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
 end
