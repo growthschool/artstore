ab4e5f423f1ba3a886ee857661aa8a45623d485e
kimberley <yoyijun@gmail.com>
2016-03-07 16:16:27 +0800

    add seed to add admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+u = User.new
+u.email = "yoyijun@gmail.com"           # 可以改成自己的 email
+
+u.password = "11111111"              # 最少要八碼
+
+u.password_confirmation = "11111111" # 最少要八碼
+
+u.is_admin = true
+u.save
