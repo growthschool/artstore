20391d4abfc51fa53894b8dcfc0b50991cf53837
JerryLu <icecaramelmilktea@yahoo.com.tw>
2016-03-07 11:37:20 +0800

    homework step7 finish ready to deploy to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :admin do
     resources :products
+    resources :users do
+      member do
+        post :to_admin
+        post :to_normal
+      end
+    end
   end
   root "products#index"
   resources :products
