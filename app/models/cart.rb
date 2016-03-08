598887f39630fc99847d3cc63df929dfdbfcb02f
sdlong <sdlong.jeng@gmail.com>
2016-03-08 16:09:58 +0800

    實作加入購物車的 model 設定

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,10 @@
 class Cart < ActiveRecord::Base
   has_many :cart_items, dependent: :destroy
   has_many :items, through: :cart_items, source: :product
+
+  def add_product_to_cart(product)
+    ci = cart_items.build
+    ci.product = product
+    ci.save
+  end
 end
