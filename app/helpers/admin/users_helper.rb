52abc505e921d4225544156a909f33934e0893e8
sdlong <sdlong.jeng@gmail.com>
2016-03-08 16:00:22 +0800

    完成設定使用者權限的功能設定

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,2 +1,9 @@
 module Admin::UsersHelper
+  def render_to_normal_user_button(user, current_user)
+    if user == current_user
+      "這是你的帳號，無法變更"
+    else
+      link_to("轉為一般使用者", to_normal_admin_user_path(user), method: :post, class: "btn btn-info")
+    end
+  end
 end
