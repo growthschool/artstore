6e3c7248179ff4719f71f94ea1c4ab24266423ef
sdlong <sdlong.jeng@gmail.com>
2016-03-08 15:34:24 +0800

    商品修改頁也能修替換圖片

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -14,6 +14,12 @@ class Admin::ProductsController < ApplicationController
 
   def edit
     @product = Product.find(params[:id])
+
+    if @product.photo.present?
+       @photo = @product.photo
+     else
+       @photo = @product.build_photo
+     end
   end
 
   def create
