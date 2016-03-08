3963e4231a864f4d379f069ee6545cbfd385e37c
Yuduki <Yuduki@gmail.com>
2016-03-08 20:53:10 +0800

    增加圖片上傳功能

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -3,12 +3,14 @@ class Admin::ProductsController < ApplicationController
   before_action :admin_required
 
 
+
   def index
     @products = Product.all
   end
 
   def new
     @product = Product.new
+    @photo = @product.build_photo
   end
 
   def create
@@ -21,9 +23,29 @@ class Admin::ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+
+    if @product.photo.present?
+      @photo = @product.photo
+    else
+      @photo = @product.build_photo
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price)
+    params.require(:product).permit(:title, :description, :quantity, :price, photo_attributes: [:image, :id])
   end
 end
