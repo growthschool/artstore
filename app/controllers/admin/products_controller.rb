1091627d537220700e7eee0b0ec052849f71fa23
黃詩婷 <shih_25@yahoo.com.tw>
2016-03-09 01:26:59 +0800

    finish

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,12 +1,39 @@
 class Admin::ProductsController < ApplicationController
+
+	layout "admin"
+
 	before_action :authenticate_user!
 	before_action :admin_required
 	def index
     @products = Product.all
   end
+
 	def new
     @product = Product.new
+    @photo = @product.build_photo
   end
+
+  def edit
+    @product = Product.find(params[:id])
+
+     if @product.photo.present?
+       @photo = @product.photo
+     else
+       @photo = @product.build_photo
+     end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
+  end
+
+
 
   def create
     @product = Product.new(product_params)
@@ -21,6 +48,7 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price)
+    params.require(:product).permit(:title, :description, :quantity, :price,
+                                    photo_attributes: [:image, :id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
+
+  root "products#index"  
+
   devise_for :users
   namespace :admin do
     resources :products
+    
+     resources :users do
+       member do
+         post :to_admin
+         post :to_normal
+       end
+     end
   end
   
-  root "welcome#index"
+  resources :products
+
+  #root "welcome#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
