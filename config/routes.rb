d1e95a008117ed64de37b07fb5b09992e3f61f97
vds <vdschiu@gmail.com>
2016-03-08 23:15:33 +0800

    建立使用者後台做權限管理_設定使用者權限 (功能設定)

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :products
-    resources :users
+    resources :users do
+      member do
+        post :to_admin
+        post :to_normal
+      end
+    end
   end
 
   resources :products
