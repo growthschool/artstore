3263c2d9e2544b054fa18009ac18b77af20adc14
sdlong <sdlong.jeng@gmail.com>
2016-03-08 15:13:09 +0800

    新增一個種子檔，可以 rake db:seed 自動建一個有 admin 權限的使用者

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+u = User.new
+
+u.email = "admin@test.com"           # 可以改成自己的 email
+u.password = "12345678"              # 最少要八碼
+u.password_confirmation = "12345678" # 最少要八碼
+
+u.is_admin = true
+u.save
