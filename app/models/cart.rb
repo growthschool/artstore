6cbce82c65dc61bd33a75e1c3b68bfa78c105699
MingFangTsai <fangsytwu@gmail.com>
2016-03-08 19:33:28 +0800

    Week 2_HW01

---
+++ app/models/cart.rb
@@ -0,0 +1,22 @@
+class Cart < ActiveRecord::Base
+  has_many :cart_items, dependent: :destroy
+  has_many :items, through: :cart_items, source: :product
+
+  def add_product_to_cart(product)
+    items << product
+  end
+
+  def total_price
+    sum = 0
+
+    items.each do |item|
+      sum = sum + item.price
+    end
+
+    return sum
+  end
+
+  def clean!
+    cart_items.destroy_all
+  end
+end
