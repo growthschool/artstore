529f47a26b3541dc185446f56f532786ab42d39b
francishuang <francis3925@gmail.com.tw>
2016-03-08 16:56:30 +0800

    fix

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,7 +3,7 @@ class Cart < ActiveRecord::Base
   has_many :items, through: :cart_items, source: :product
 
   def add_product_to_cart(product)
-    ci = cart.cart_items.build
+    ci = cart_items.build
     ci.product = product
     ci.save
   end
