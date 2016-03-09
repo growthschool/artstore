8cee32707be93cb0f6df9cceafa928e683218611
Chao-Yee Hsu <chaoyee22@gmail.com>
2016-03-09 20:32:57 +0800

    購物車及其商品明細

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,13 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :products
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
+
+  resources :carts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
