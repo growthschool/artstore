6ba16f678480c47e939abfd8e1f2b6ab6e38fc63
yoyyotang <yoyyotang@gmail.com>
2016-03-10 00:08:54 +0800

    store-v2-homework deploy

--- app/models/order.rb
+++ app/models/order.rb
@@ -17,7 +17,7 @@ accepts_nested_attributes_for :info
     cart.items.each do |cart_item|
       item = items.build
       item.product_name = cart_item.title
-      item.quantity = 1
+      item.quantity = cart.find_cart_item(cart_item).quantity
       item.price = cart_item.price
       item.save
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  namespace :account do
+    resources :orders
+  end
+
+  resources :items, controller: "cart_items"
+
   devise_for :users
 
   resources :orders do
@@ -13,11 +19,22 @@ Rails.application.routes.draw do
   resources :carts do
     collection do
       post :checkout
+      delete :clean
     end
   end
 
   namespace :admin do
     resources :products
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end
+
+
      resources :users do
        member do
          post :to_admin
@@ -31,7 +48,6 @@ Rails.application.routes.draw do
       post :add_to_cart
     end
   end
-
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
