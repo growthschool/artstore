ad1f6e332eeadf39f315c786293eff297cedc5b2
Yuduki <Yuduki@gmail.com>
2016-03-10 21:16:28 +0800

    訂單可按照狀態圖改變狀態

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -2,4 +2,12 @@ module OrdersHelper
   def render_order_state(order)
     t("orders.order_state.#{order.aasm_state}")
   end
+
+  def render_order_paid_state(order)
+    if order.is_paid?
+      "已付款"
+    else
+      "未付款"
+    end
+  end
 end
