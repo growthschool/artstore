de8e6ff320de6ad5885137150258ed75254fb457
本名 <futp3249@hotmail.com>
2016-03-10 20:10:41 +0800

    w2-2

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,14 @@ resources :carts do
 
   namespace :admin do
     resources :products
-    resources :orders
+    resources :orders do
+     member do
+       post :cancel
+       post :ship
+       post :shipped
+       post :return
+     end
+   end
    resources :users do
      member do
         post :to_admin
