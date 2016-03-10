b3b010bd4a6ae61a7b78ea9e9f7aa52221fa88ae
francishuang <francis3925@gmail.com.tw>
2016-03-10 14:22:05 +0800

    upload pic when create & edit

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -13,6 +13,12 @@ class Admin::ProductsController < ApplicationController
 
   def edit
     @product = Product.find(params[:id])
+
+    if @product.photo.present?
+      @photo = @product.photo
+    else
+      @photo = @product.build_photo
+    end
   end
 
   def update
@@ -38,7 +44,8 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price)
+    params.require(:product).permit(:title, :description, :quantity, :price,
+                                     photo_attributes: [:image, :id])
   end
 
 end
