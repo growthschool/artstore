c856eb6abed79a5daaab75c9f26510b35cad452f
Daniel Lin <zarr12steven@hotmail.com>
2016-03-12 10:27:14 +0800

    將訂單結帳

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,5 @@
 module ProductsHelper
+  def render_cart_items_count(cart)
+    cart.items.count
+  end
 end
---
+++ app/models/order.rb
@@ -0,0 +1,37 @@
+class Order < ActiveRecord::Base
+    belongs_to :user
+
+    has_many :items, class_name: "OrderItem", dependent: :destroy
+    has_one  :info,  class_name: "OrderInfo", dependent: :destroy
+
+    accepts_nested_attributes_for :info
+
+    before_create :generate_token
+
+    def generate_token
+        self.token = SecureRandom.uuid
+    end
+
+    def build_item_cache_from_cart(cart)
+      cart.items.each do |cart_item|
+        item = items.build
+        item.product_name = cart_item.title
+        item.quantity = 1
+        item.price = cart_item.price
+        item.save
+      end
+    end
+
+    def calculate_total!(cart)
+      self.total = cart.total_price
+      self.save
+    end
+
+    def set_payment_with!(method)
+        self.update_columns(payment_method: method)
+    end
+
+    def pay!
+        self.update_columns(is_paid: true)
+    end
+end
