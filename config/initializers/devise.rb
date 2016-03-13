442add214e8df92481df310d504b895aa4153afe
wildjcrt <wildjcrt@yahoo.com.tw>
2016-03-14 03:33:51 +0800

    設定 Facebook App ID 和 Secret

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -235,7 +235,8 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, '1711139452497840', '70e58b10a440f9125bd67e3c6e09e75b',
+                  :scope => 'email, publish_actions'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
