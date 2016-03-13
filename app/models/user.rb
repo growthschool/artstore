afd66584be09846252b1b5bca5d67ff7d1a124b8
wildjcrt <wildjcrt@yahoo.com.tw>
2016-03-14 04:56:48 +0800

    紀錄來自 Facebook 登入的資訊

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,13 @@ class User < ActiveRecord::Base
 
   has_many :orders
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+
   def admin?
     is_admin
   end
