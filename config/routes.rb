aa44aba2836faca1ffce0170776a7bdf3699383d
wildjcrt <wildjcrt@yahoo.com.tw>
2016-03-14 04:41:20 +0800

    routes 設定 callback controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "products#index"
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   namespace :admin do
     resources :products
 
