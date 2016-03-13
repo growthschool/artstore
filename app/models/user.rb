65e8b9e06da817071c27c2b7dbea29d7c4edb3c0
wildjcrt <wildjcrt@yahoo.com.tw>
2016-03-14 04:35:50 +0800

    新增 omniauthable 設定，讓 User model 可以接受 facebook 登入選項。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, :omniauth_providers => [:facebook]
 
   has_many :orders
 
