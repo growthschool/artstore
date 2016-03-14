137c1014c7f7616cc3269edeab68650eb31c0cf0
yushengc <ntuyusheng@gmail.com>
2016-03-14 20:07:50 +0800

    week 2 hw finalize

--- config/routes.rb
+++ config/routes.rb
@@ -22,20 +22,26 @@ Rails.application.routes.draw do
  end
 
   namespace :admin do
-	resources :products
-  resources :orders
-	resources :users do
-		member do
-			post :to_admin
-			post :to_normal
-		end
-	end
+  	resources :products
+    resources :orders do
+        member do
+          post :cancel
+          post :ship
+          post :shipped
+          post :return
+        end
+    end
+
+  	resources :users do
+  		member do
+  			post :to_admin
+  			post :to_normal
+  		end
+  	end
   end
 
   namespace :account do
     resources :orders
-
-
   end
   resources :items, controller: "cart_items"
 
