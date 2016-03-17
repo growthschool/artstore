437ebca2de82313140f2db8ed1ce38b03c9066b5
francishuang <francis3925@gmail.com.tw>
2016-03-17 16:09:38 +0800

    add total price to cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -11,11 +11,11 @@ class Cart < ActiveRecord::Base
   def total_price
     sum = 0
 
-    items.each do |item|
-      sum = sum + item.price
+    cart_items.each do |cart_item|
+      sum = sum + (cart_item.product.price * cart_item.quantity)
     end
 
-    sum
+    return sum
   end
 
   def clean!
