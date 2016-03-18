6f3f241d230a87c391ba835a1f73d15d29119e6a
sdlong <sdlong.jeng@gmail.com>
2016-03-18 12:57:49 +0800

    pay2go 設定

---
+++ config/initializers/pay2go.rb
@@ -0,0 +1,13 @@
+Pay2go.setup do |pay2go|
+  if Rails.env.development?
+    pay2go.merchant_id = "1974096"
+    pay2go.hash_key    = "siQtMNJ3fiDeAdkTOoxrGGIEGrKBXnaq"
+    pay2go.hash_iv     = "G0UtOEw3WaiwnCqc"
+    pay2go.service_url = "https://capi.pay2go.com/MPG/mpg_gateway"
+  else
+    pay2go.merchant_id = "1974096"
+    pay2go.hash_key    = "siQtMNJ3fiDeAdkTOoxrGGIEGrKBXnaq"
+    pay2go.hash_iv     = "G0UtOEw3WaiwnCqc"
+    pay2go.service_url = "https://api.pay2go.com/MPG/mpg_gateway"
+  end
+end
