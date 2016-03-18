cde504bc840d06d9f50eac5ca8fa8530d4dc0102
sdlong <sdlong.jeng@gmail.com>
2016-03-18 14:54:02 +0800

    修改 heroku domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'desolate-meadow-72416.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'artstore-sample.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
