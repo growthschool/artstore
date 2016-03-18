643868e5cca2306e9b5c5959531876c4b48aac88
Chad Yeh <chadyeh@MacBook-Pro-2.local>
2016-03-18 08:18:33 +0800

    carts/checkout增加購買數量與購物車沒有商品無法結帳

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,8 +2,13 @@ class CartsController < ApplicationController
   before_action :authenticate_user!, only: [:checkout]
 
   def checkout
-    @order = current_user.orders.build
-    @info = @order.build_info
+    if current_cart.cart_items.count > 0
+      @order = current_user.orders.build
+      @info = @order.build_info
+    else
+      flash[:warning] = "購物車內沒有物品"
+      redirect_to carts_path
+    end
   end
 
   def clean
