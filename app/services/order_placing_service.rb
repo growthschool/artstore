092c3b836a4ff85b5dc459d2bf036eef6211d951
sdlong <sdlong.jeng@gmail.com>
2016-03-18 14:47:40 +0800

    用 service_object 整理 訂單建立時的相關設定

---
+++ app/services/order_placing_service.rb
@@ -0,0 +1,13 @@
+class OrderPlacingService
+  def initialize(cart, order)
+    @order = order
+    @cart = cart
+  end
+
+  def place_order!
+    @order.build_item_cache_from_cart(@cart)
+    @order.calculate_total!(@cart)
+    @cart.clean!
+    # OrderMailer.notify_order_placed(@order).deliver! # mailgun 寄信功能設定完成後再啟用
+  end
+end
