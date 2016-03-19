caa1b01a600026e263af8248641f25612ffed613
rjjq <rrrjjjqqq@gmail.com>
2016-03-19 16:42:56 +0800

    第二週上課: 4.建立訂單 (2.生成訂單)

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,18 @@ class Order < ActiveRecord::Base
   has_one :info, class_name: "OrderInfo", dependent: :destroy
 
   accepts_nested_attributes_for :info
+
+  def build_item_cache_from_cart(cart)
+    cart.items.each do |product|
+      item = items.build
+      item.product_name = product.title
+      item.quantity = 1
+      item.price = product.price
+      item.save
+    end
+  end
+
+  def calculate_total!(cart)
+    self.update_columns(total: cart.total_price)
+  end
 end
