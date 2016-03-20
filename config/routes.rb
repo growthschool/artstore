1854fd1093b12109dedaf76fcd5ef0a1a9a35991
kurotyan514 <kurotyan514@gmail.com>
2016-03-20 20:29:12 +0800

    建立 account/orders 可以看到該使用者過去所有訂單

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,10 @@ Rails.application.routes.draw do
     end
   end
 
+  namespace :account do
+    resources :orders
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
