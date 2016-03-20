4301602dd421cfd00aefbe3fd6551de67dabdfb4
rjjq <rrrjjjqqq@gmail.com>
2016-03-20 21:37:04 +0800

    第二週作業: #1-4 購物車加入「數量」設計(總價 => 小計 x 數量)

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -9,8 +9,8 @@ class Cart < ActiveRecord::Base
   def total_price
     sum = 0
 
-    items.each do |product|
-      sum = sum + product.price
+    cart_items.each do |cart_item|
+      sum = sum + (cart_item.product.price * cart_item.quantity)
     end
 
     return sum
