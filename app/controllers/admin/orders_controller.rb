809ba4068a1f398f0acc7a0d47ccd548ba0f50e3
kurotyan514 <kurotyan514@gmail.com>
2016-03-21 23:57:54 +0800

    單張 order 應要可以依「按照狀態圖」改變狀態

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,43 @@
 class Admin::OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :admin_required
+  before_action :find_order , only:[:ship,:shipped,:cancel,:return]
+
   def index
     @orders = Order.order("id DESC")
+  end
+
+  def show
+    @order = Order.find(params[:id])
+    @order_info = @order.info
+    @order_items = @order.items
+  end
+
+  def ship
+
+    @order.ship!
+    redirect
+  end
+  def shipped
+    @order.deliver!
+    redirect
+  end
+  def cancel
+    @order.cancell_order!
+    redirect
+  end
+  def return
+    @order.return_good!
+    redirect
+  end
+
+
+  private
+  def find_order
+    @order = Order.find(params[:id])
+  end
+
+  def redirect
+    redirect_to :back
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Artstore
   class Application < Rails::Application
-    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -19,8 +19,8 @@ module Artstore
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
+    
+    config.i18n.default_locale = "zh-TW"
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
