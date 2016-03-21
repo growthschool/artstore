d3d52f6cd224e3da0b198385e971f345e7d36091
Daniel <oopdaniel@gmail.com>
2016-03-22 01:12:19 +0800

    solve the damn bug...

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,13 +14,13 @@ class OrdersController < ApplicationController
   end
 
   def show
-    @order = Order.find_by_token(params[:id])
+    @order = Order.find_by(token: params[:id])
     @order_info = @order.info
     @order_items = @order.items
   end
 
   def pay_with_credit_card
-    @order = Order.find_by_token(params[:id])
+    @order = Order.find_by(token: params[:id])
     @order.set_payment_with!("credit_card")
     @order.make_payment!
 
--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   resources :carts do
     collection do
       post :checkout
+      delete :clean
     end
   end
 
@@ -29,6 +30,8 @@ Rails.application.routes.draw do
       get :pay_with_credit_card
     end
   end
+
+  resources :items, controller: "cart_items"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
