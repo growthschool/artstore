fd540bf4835e63fcef2996366e509531d589661b
rjjq <rrrjjjqqq@gmail.com>
2016-03-21 22:23:01 +0800

    第三週上課: 3.Deploy 到 Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.after_initialize do
-    Pay2go.integration_mode = :production
+    Pay2go.integration_mode = :development
   end
+
+  config.action_mailer.default_url_options = { host: 'https://young-reef-24730.herokuapp.com' }
 end
