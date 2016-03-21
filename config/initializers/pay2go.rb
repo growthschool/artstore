33763289665d6e64b0b70de2074286d09883559f
francishuang <francis3925@gmail.com.tw>
2016-03-21 22:32:48 +0800

    fix pay2go.rb

--- config/initializers/pay2go.rb
+++ config/initializers/pay2go.rb
@@ -5,9 +5,5 @@ Pay2go.setup do |pay2go|
     pay2go.hash_iv     = "aWpfjFljWjet5ZhX"
     pay2go.service_url = "https://capi.pay2go.com/MPG/mpg_gateway"
   else
-    pay2go.merchant_id = "1234567"  # 放正式站的 key
-    pay2go.hash_key    = "xxxxxxxx"
-    pay2go.hash_iv     = "xxxxxxxx"
-    pay2go.service_url = "https://api.pay2go.com/MPG/mpg_gateway"
   end
 end
