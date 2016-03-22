0da4d321b8af25bb875b3c04e347738789016c9f
Daniel <oopdaniel@gmail.com>
2016-03-23 00:25:14 +0800

    fin

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -11,4 +11,23 @@ class CartItemsController < ApplicationController
     redirect_to :back
   end
 
+  def update
+    @cart = current_cart
+    @item = @cart.find_cart_item(params[:id])
+
+    if @item.product.quantity >= item_params[:quantity].to_i
+      @item.update(item_params)
+      flash[:notice] = "成功變更數量"
+    else
+      flash[:warning] = "數量不足以加入購物車"
+    end
+
+    redirect_to carts_path
+  end
+
+  private
+
+  def item_params
+    params.require(:cart_item).permit(:quantity)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,14 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :admin do
     resources :products
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end
     resources :users do
       member do
         post :to_admin
@@ -29,6 +37,10 @@ Rails.application.routes.draw do
     member do
       get :pay_with_credit_card
     end
+  end
+
+  namespace :account do
+    resources :orders
   end
 
   resources :items, controller: "cart_items"
