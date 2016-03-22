65947ab807eaee402aa9c6c30b2d4bac67b83eb8
francishuang <francis3925@gmail.com.tw>
2016-03-22 17:55:57 +0800

    tokenable.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,16 +1,13 @@
 class Order < ActiveRecord::Base
+
+  include Tokenable
+
   belongs_to :user
 
   has_many :items, class_name: "OrderItem", dependent: :destroy
   has_one  :info,  class_name: "OrderInfo", dependent: :destroy
 
   accepts_nested_attributes_for :info
-
-  before_create :generate_token
-
-  def generate_token
-    self.token = SecureRandom.uuid
-  end
 
   def build_item_cache_from_cart(cart)
     cart.items.each do |cart_item|
