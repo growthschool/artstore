360e44a040b938466fd31d1393ecdefe93ebcafe
Daniel <oopdaniel@gmail.com>
2016-03-24 02:58:56 +0800

    pushing to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.after_initialize do
-    Pay2go.integration_mode = :production
+    Pay2go.integration_mode = :development
   end
+
+  config.action_mailer.default_url_options = { host: 'blooming-fortress-10659.herokuapp.com' } # 你的 heroku app 網址
 end
