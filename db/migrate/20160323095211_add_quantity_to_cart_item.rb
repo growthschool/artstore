66595024ea72c47d674058d076998b99c5abfa2a
sylan9224 <ryan810204@hotmail.com.tw>
2016-03-23 18:08:27 +0800

    Initial Commit

---
+++ db/migrate/20160323095211_add_quantity_to_cart_item.rb
@@ -0,0 +1,6 @@
+class AddQuantityToCartItem < ActiveRecord::Migration
+  def change
+   #add_column :cart_items, :quantity, :integer
+    add_column :cart_items, :quantity, :integer, default: 1
+  end
+end
