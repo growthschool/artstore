8bc114891b8246064866af2dbccfb9e4dad486f3
Daniel <oopdaniel@gmail.com>
2016-03-24 02:45:53 +0800

    figaro

--- Gemfile
+++ Gemfile
@@ -52,6 +52,8 @@ gem "roadie", "2.4.3"
 
 gem "pay2go"
 
+gem "figaro"
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- config/initializers/pay2go.rb
+++ config/initializers/pay2go.rb
@@ -1,15 +1,6 @@
 Pay2go.setup do |pay2go|
-  if Rails.env.development?
-    pay2go.merchant_id = "11696492"  # 放測試站的 key
-
-    pay2go.hash_key    = "YpIGrwazcLj3AjRyf3jx3puef5WNVr2U"
-    pay2go.hash_iv     = "QKAihLRuxbx7vpFK"
-    pay2go.service_url = "https://capi.pay2go.com/MPG/mpg_gateway"
-  else
-    pay2go.merchant_id = "11696492"  # 放正式站的 key
-
-    pay2go.hash_key    = "xxxxxxxx"
-    pay2go.hash_iv     = "xxxxxxxx"
-    pay2go.service_url = "https://api.pay2go.com/MPG/mpg_gateway"
-  end
+  pay2go.merchant_id = ENV['pay2go_merchant_id']
+  pay2go.hash_key    = ENV['pay2go_hash_key']
+  pay2go.hash_iv     = ENV['pay2go_hash_iv']
+  pay2go.service_url = ENV['pay2go_service_url']
 end
