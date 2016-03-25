e4a8ce778550c472d622538130ad28cafefb388c
francishuang <francis3925@gmail.com.tw>
2016-03-25 14:27:32 +0800

    S3 fog figaro

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,16 @@
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_credentials = {
+      provider:              'AWS'
+      aws_access_key_id:     ENV['S3_key_id'],
+      aws_secret_access_key: ENV['S3_key_secret'],
+      region:                'ap-southeast-1'
+    }
+    config.fog_directory = 'francis-bucket'
+
+  else
+    config.storage :file
+  end
+
+end
