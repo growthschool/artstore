6dd302dbed452ff0cc9024f504e8fe3d6b61a57f
Chad Yeh <chadyeh@MacBook-Pro-2.local>
2016-03-27 22:20:21 +0800

    用 concerns 整理 before_create

--- app/models/order.rb
+++ app/models/order.rb
@@ -21,11 +21,7 @@ class Order < ActiveRecord::Base
     self.save
   end
 
-  before_create :generate_token
-
-  def generate_token
-    self.token = SecureRandom.uuid
-  end
+  include Tokenable
 
   def set_payment_with!(method)
     self.update_columns(payment_method: method )
