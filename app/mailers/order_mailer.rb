2286d20ceae06e48895a90218d56b4eb43a1976d
Chad Yeh <chadyeh@Chads-MacBook-Pro.local>
2016-03-28 01:41:13 +0800

    edit email subject

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -5,6 +5,6 @@ class OrderMailer < ApplicationMailer
     @order_items = @order.items
     @order_info  = @order.info
 
-    mail(to: @user.email , subject: "[Artstore] 感謝您完成本次的下單，以下是您這次購物明細 #{order.token}")
+    mail(to: @user.email , subject: "[Chadstore] 感謝您完成本次的下單，以下是您這次購物明細 #{order.token}")
   end
 end
