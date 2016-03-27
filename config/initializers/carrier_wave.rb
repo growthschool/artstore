7dbb14eabb4940ceeccbfd02de1f6d19046e7f4c
oldgi <chinyu.tsai@gmail.com>
2016-03-27 18:19:34 +0800

    Finish Mail S3 and Domain for Heroku

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,12 +2,12 @@ CarrierWave.configure do |config|
   if Rails.env.production? 
     config.storage :fog                       
     config.fog_credentials = {
-      provider:              ENV['cloud_provider'],                        
-      aws_access_key_id:     ENV['aws_access_key_id'],      # 你的 key           
+      provider:              'AWS',                        
+      aws_access_key_id:     'AKIAICZ7FFIG6QKIPDFA',      # 你的 key           
 
-      aws_secret_access_key: ENV['aws_secret_access_key'],      # 你的 secret key         
+      aws_secret_access_key: 'U77rO0lEeYdMVGW5j0ZV1dnAbEs4R8SUvPsdNwGd',      # 你的 secret key         
 
-      region:                ENV['cloud_region'] # 你的 S3 bucket 的 Region 位置   
+      region:                'Oregon' # 你的 S3 bucket 的 Region 位置   
 
     }
     config.fog_directory  = 's3wahaha3s' # 你設定的 bucket name 
