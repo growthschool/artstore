6b075b18673060a61b437630d5a183e860cfc2b0
Chad Yeh <chadyeh@Chads-MacBook-Pro.local>
2016-03-28 00:05:43 +0800

    add pay2go ATM function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: :pay2go_cc_notify
+  before_action :authenticate_user!, except: [:pay2go_cc_notify, :pay2go_atm_complete]
 
-  protect_from_forgery except: :pay2go_cc_notify
+  protect_from_forgery except: [:pay2go_cc_notify, :pay2go_atm_complete]
 
   def create
     @order = current_user.orders.build(order_params)
@@ -45,6 +45,23 @@ class OrdersController < ApplicationController
     @order.make_payment!
     redirect_to account_orders_path, notice: "成功完成付款"
   end
+
+  def pay2go_atm_complete
+    @order = Order.find_by_token(params[:id])
+
+    json_data = JSON.parse(params["JSONData"])
+
+    if json_data["Status"] == "SUCCESS"
+
+      @order.set_payment_with!("atm")
+      @order.make_payment!
+
+      render text: "交易成功"
+    else
+      render text: "交易失敗"
+    end
+  end
+
 
   private
 
