ab82e4a1a3cf149bd4dfd403f147153ea7bba401
oldgi <chinyu.tsai@gmail.com>
2016-03-27 19:57:11 +0800

    Finish ATM

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ CarrierWave.configure do |config|
     config.storage :fog                       
     config.fog_credentials = {
       provider:              'AWS',                        
-      aws_access_key_id:     ENV["aws_access_key_id"],      # 你的 key           
+      aws_access_key_id:     ENV['aws_access_key_id'],      # 你的 key           
 
-      aws_secret_access_key: ENV["aws_secret_access_key"],      # 你的 secret key         
+      aws_secret_access_key: ENV['aws_secret_access_key'],      # 你的 secret key         
 
       region:                'Oregon' # 你的 S3 bucket 的 Region 位置   
 
