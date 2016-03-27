3562286cb3973eed3a6fb212a1af926f5cec7896
Chad Yeh <chadyeh@Chads-MacBook-Pro.local>
2016-03-27 23:20:55 +0800

    完成 production 寄信設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,16 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'chadstore.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:           587,
+    address:        "smtp.mailgun.org",
+    user_name:      ENV["mailgun_user"],
+    password:       ENV["mailgun_secret"],
+    domain:         "sandbox59cd0106163d459e841b0c0111735c5e.mailgun.org", # 你的 mailgun domain name
+
+    authentication: :plain,
+  }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
