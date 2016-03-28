d48cd864559951b1a8cb57b7de3ca86ee22796f0
francishuang <francis3925@gmail.com.tw>
2016-03-28 17:21:43 +0800

    set controller facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,13 @@ class User < ActiveRecord::Base
     self.update_columns(is_admin: false)
   end
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+
   has_many :orders
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   namespace :admin do
     resources :products
