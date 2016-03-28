f790fe35e48860f13d9c410aefe8a6fb6bdd30f5
Chad Yeh <chadyeh@Chads-MacBook-Pro.local>
2016-03-28 20:05:59 +0800

    debug

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
 
       aws_secret_access_key: ENV['aws_secret_access_key'],      # 你的 secret key
 
-      region:                'Tokyo' # 你的 S3 bucket 的 Region 位置
+      region:                'ap-northeast-1' # 你的 S3 bucket 的 Region 位置
 
     }
     config.fog_directory  = ENV['fog_directory'] # 你設定的 bucket name
