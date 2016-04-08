dcc4a4bc17507440d300864f6bc92526618cc272
陳樂樂 <jmulkort@gmail.com>
2016-04-08 21:46:37 +0800

    deploy to heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-         def admin?
-   is_admin
+  def admin?
+    is_admin
+  end
+
+   def to_admin
+   self.update_columns(is_admin: true)
+ end
+
+ def to_normal
+   self.update_columns(is_admin: false)
  end
 end
