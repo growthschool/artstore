267171c3fbe4efe98830b01b8a26836a927554c5
Hsuan-Cheng, Chen <poligen@gmail.com>
2016-04-12 22:47:05 +0800

    finish week 1 homework

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ActiveRecord::Base
+  has_one :photo
+  accepts_nested_atrribute_for :photo
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :admin do
     resources :products
-
+    resources :users do
+      member do
+        post :to_admin
+        post :to_normal
+      end
+    end
   end
+  resources :products
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
