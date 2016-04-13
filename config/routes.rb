57441ef3df2bc1f759554140a4c8e1c677d8eb45
anxgang@gmail.com <anxgang@gmail.com>
2016-04-13 21:24:06 +0800

    建立購物清單

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ Rails.application.routes.draw do
        post :add_to_cart
     end
   end
+
+  resources :carts
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
