0d9ddb01c38a6cdf50e430a03157b6e17048306f
hgmtaiwan <hgmtaiwan@gmail.com>
2016-04-14 05:29:09 +0800

    改完bug

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,8 @@
 class Admin::UsersController < ApplicationController
   layout "admin"
+
+  before_action :authenticate_user!
+  before_action :admin_required
   def index
     @users = User.all
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+u = User.new
+u.email = "admin@test.com"
+u.password = "12345678"
+u.password_confirmation = "12345678"
+u.is_admin = true
+u.save
