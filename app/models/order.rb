936955852ab01bd176f82ff3bbb99d60670704c9
Poligen Chen <poligen@gmail.com>
2016-04-14 20:39:17 +0800

    finish order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,21 @@
 class Order < ActiveRecord::Base
+  belongs_to :user
+  has_many :items, class_name: "OrderItem", dependent: :destroy
+  has_one :info, class_name: "OrderInfo", dependent: :destroy
+
+  accepts_nested_attributes_for :info
+
+  def build_item_cache_from_cart(cart)
+    cart.items.each do |cart_item|
+      item.product_name = cart_item.title
+      item.quantity = 1
+      item.price = cart_item.price
+      item.save
+    end
+  end
+
+  def calculate_total!(cart)
+    self.total = cart.total_price
+    self.save
+  end
 end
