8495fb3e72a7ed8dc0d6ab020fddf036fed037d0
DingDing <advancedor96@gmail.com>
2016-04-14 16:21:38 +0800

    完成作業1：加入bootstrap的navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
 
+  root "products#index"
   devise_for :users
   namespace :admin do
     resources :products
   end
 
+  resources :products
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
