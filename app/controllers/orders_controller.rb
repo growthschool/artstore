748f70b233163c93eba3e072f8674a5b1cd25650
bohung <monkeytai3@gmail.com>
2016-04-15 16:34:06 +0800

    第二週作業進度

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,11 +5,12 @@ class OrdersController < ApplicationController
     @order = current_user.orders.build(order_params)
 
     if @order.save
-      @order.build_item_cache_from_cart(current_cart)
-      @order.calculate_total!(current_cart)
-      redirect_to order_path(@order.token)
+       @order.build_item_cache_from_cart(current_cart)
+       @order.calculate_total!(current_cart)
+       current_cart.clean!
+       redirect_to order_path(@order.token)
     else
-      render "carts/checkout"
+       render "carts/checkout"
     end
   end
 
@@ -24,7 +25,7 @@ class OrdersController < ApplicationController
     @order.set_payment_with!("credit_card")
     @order.make_payment!
 
-    redirect_to order_path(@order.token), notice: "成功完成付款"
+    redirect_to account_orders_path, notice: "成功完成付款"
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,17 +6,32 @@ Rails.application.routes.draw do
     end
   end
 
+  namespace :account do
+   resources :orders
+  end
+
+  resources :items, controller: "cart_items"
+
   root "products#index"
 
    resources :carts do
      collection do
        post :checkout
+       delete :clean
      end
    end
 
   devise_for :users
   namespace :admin do
    resources :products
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end
      resources :users do
        member do
          post :to_admin
