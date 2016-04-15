f45b467358d07fae0c9894e23a9ebf309bcd66f7
peiyupan <fishfish711@gmail.com>
2016-04-15 17:40:18 +0800

    第二週作業完成

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,21 @@ Rails.application.routes.draw do
    resources :carts do
      collection do
        post :checkout
+       delete :clean
      end
    end
 
   devise_for :users
   namespace :admin do
     resources :products
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end
     resources :users do
       member do
         post :to_admin
@@ -28,6 +37,12 @@ Rails.application.routes.draw do
      member do
        get :pay_with_credit_card
      end
+   end
+
+   resources :items, controller: "cart_items"
+
+   namespace :account do
+     resources :orders
    end
 
   # The priority is based upon order of creation: first created -> highest priority.
