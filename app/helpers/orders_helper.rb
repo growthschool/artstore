1ae2d0d75c1052195c87068ac12d82e493fb3901
Steven Chen <steven.chen4hao@gmail.com>
2016-04-17 20:32:46 +0800

    單張 order 應要可以依「按照狀態圖」改變狀態

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -3,4 +3,12 @@ module OrdersHelper
     t("orders.order_state.#{order.aasm_state}")
   end
 
+  def render_order_paid_state(order)
+    if order.is_paid?
+      "已付款"
+    else
+      "未付款"
+    end
+  end
+
 end
