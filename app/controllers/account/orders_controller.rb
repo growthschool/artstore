4f851b8a4d77824cb77a18e149bb3cae7aff4e6f
Poligen Chen <poligen@gmail.com>
2016-04-21 17:00:10 +0800

    fix bug

--- app/controllers/account/orders_controller.rb
+++ app/controllers/account/orders_controller.rb
@@ -1,5 +1,5 @@
 class Account::OrdersController < ApplicationController
-  beofrore_action :authenticate_user!
+  before_action :authenticate_user!
 
   def index
     @orders = current_user.orders.order("id DESC")
