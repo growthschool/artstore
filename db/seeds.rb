19e835d70b449db02d60d5076bb96025907af2f6
nn90121 <nn90121@gmail,com>
2016-04-24 20:35:28 +0800

    create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+admin = User.new(email: "admin@gmail.com", password: "password", password_confirmation: "password", is_admin: true)
