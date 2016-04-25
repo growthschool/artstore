e0c8998721203f21f385d8db2ee903bfd2666a27
advancedor96 <advancedor96@gmail.com>
2016-04-25 17:23:58 +0800

    做完第二週上課進度

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,9 @@
+class CartsController < ApplicationController
+	before_action :authenticate_user!, only: [:checkout]
+
+	def checkout
+	  @order = current_user.orders.build
+	  @info = @order.build_info
+	end
+	
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,24 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :products
+  resources :orders do
+    member do
+      get :pay_with_credit_card
+    end
+  end
+  
+
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
+
+  resources :carts do
+    collection do
+      post :checkout
+    end
+  end  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
