a8f22d3fc2c0233db0d88c8a4debcd80bc67641e
louiscool <louisboyili@gmail.com.tw>
2016-05-11 00:28:44 +0800

    deploy to heroku

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,26 @@
+class Admin::UsersController < ApplicationController
+
+  layout "admin"
+
+  before_action :authenticate_user!
+  before_action :admin_required
+
+  def index
+    @users = User.all
+  end
+
+
+  def to_admin
+    @user = User.find(params[:id])
+    @user.to_admin
+
+    redirect_to admin_users_path
+  end
+
+  def to_normal
+    @user = User.find(params[:id])
+    @user.to_normal
+
+    redirect_to admin_users_path
+  end
+end
