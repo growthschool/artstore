90fa09d98be6a0955449ef046ab8d0f24a0a6591
LizHuang <search.psop@gmail.com.tw>
2016-05-10 19:03:15 +0800

    在 console 建立使用者並改為 admin 權限

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+u = User.new
+u.email = "admin@test.com"           # 可以改成自己的 email
+
+u.password = "12345678"              # 最少要八碼
+
+u.password_confirmation = "12345678" # 最少要八碼
+
+u.is_admin = true
+u.save
