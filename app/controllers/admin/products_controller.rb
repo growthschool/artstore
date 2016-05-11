45a2df20005fac04c14c906e613ba485bb0f4853
LizHuang <search.psop@gmail.com.tw>
2016-05-11 22:47:52 +0800

    實作圖片上傳 carrierwave

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -9,10 +9,17 @@ class Admin::ProductsController < ApplicationController
 
   def new
     @product = Product.new
+    @photo = @product.build_photo
   end
 
   def edit
     @product = Product.find(params[:id])
+
+    if @product.photo.present?
+       @photo = @product.photo
+    else
+       @photo = @product.build_photo
+    end
   end
 
   def update
@@ -38,6 +45,7 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price)
+    params.require(:product).permit(:title, :description, :quantity, :price,
+                                     photo_attributes: [:image, :id])
   end
 end
