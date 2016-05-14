fc7d48248db8b4a5792b7d5f6ed65c7a7360e098
lyenliang <lyenliang@gmail.com>
2016-05-14 22:07:27 +0800

    Finish adding navbar

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,13 @@ Rails.application.routes.draw do
     resources :products
   end
 
+  resources :products
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'products#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
