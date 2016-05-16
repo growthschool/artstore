7045ba217074ad2c39d6c9f2b8671d8a8bb6b0ad
wowdyaln <rogersmk@gmail.com.tw>
2016-05-16 22:01:37 +0800

    上課f.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,18 @@ Rails.application.routes.draw do
 
   root "products#index"
 
+  resources :orders do
+     member do
+        get :pay_with_credit_card
+      end
+    end
+
+  resources :carts do
+    collection do
+      post :checkout
+    end
+  end
+
   resources :products do
     member do
       post :add_to_cart
---
+++ db/migrate/20160516134202_add_is_paid_to_order.rb
@@ -0,0 +1,6 @@
+class AddIsPaidToOrder < ActiveRecord::Migration
+  def change
+   add_column :orders, :is_paid, :boolean, default: false
+
+  end
+end
