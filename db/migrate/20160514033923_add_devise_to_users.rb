b252f10c74a5f2f8dc4ba0b2d2a3ee0afbe68004
Yi-Chen,Lin <hihi20256@gmail.com>
2016-05-16 18:14:18 +0800

    deploy to heroku

---
+++ db/migrate/20160514033923_add_devise_to_users.rb
@@ -0,0 +1,8 @@
+class AddDeviseToUsers < ActiveRecord::Migration
+  
+  def self.down
+    # By default, we don't want to make any assumption about how to roll back a migration when your
+    # model already existed. Please edit below which fields you would like to remove in this migration.
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+u = User.new
+u.email = "admin@test.com"
+u.password = "12345678"
+u.password_confirmation = "12345678"
+u.is_admin = true
+u.save
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
