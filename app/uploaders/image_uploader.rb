0daba81047996d4f50bdc5311639c572cabd730a
GavinChiang <cchsuan@umich.edu>
2016-05-16 14:17:37 +0800

    uploader modified

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -19,11 +19,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_fit: [800, 800]
 
   version :thumb do
-    process resize_to_fill: [200, 200]
+    process resize_to_fit: [200, 200]
   end
 
   version :medium do
-    process resize_to_fill: [400, 400]
+    process resize_to_fit: [400, 400]
   end
 
 
