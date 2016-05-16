bbf6c3110a739afb91b0eaeb34369d4c8516e310
GavinChiang <cchsuan@umich.edu>
2016-05-17 01:36:05 +0800

    week2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,37 @@
 Rails.application.routes.draw do
 
+  resources :items, controller: "cart_items"
+
+  namespace :account do
+    resources :orders
+  end
+
+  resources :orders do
+      member do
+        get :pay_with_credit_card
+      end
+    end
+  
   root "products#index"
+
+  resources :carts do
+    collection do
+      post :checkout
+      delete :clean
+    end
+  end
 
   devise_for :users
   namespace :admin do
     resources :products
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end
     resources :users do
       member do
         post :to_admin
@@ -13,7 +40,11 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :products
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
