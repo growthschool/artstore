6b2933f433c94005ba1f71d9b5efdf98e87298e7
Tien-Yi Chi <tien-yi.chi@logicsolutions.com>
2016-05-16 19:13:28 +0800

    Routes & Controller for products

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
   namespace :admin do
     resources :products
     resources :users do
@@ -15,7 +16,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'products#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
