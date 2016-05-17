b01693ef969272b9683465822411b5d521f27e97
LizHuang <search.psop@gmail.com.tw>
2016-05-18 00:32:44 +0800

    訂單產生時，要把購物車的商品數量存進去

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,6 +2,10 @@ class Cart < ActiveRecord::Base
 	has_many :cart_items, dependent: :destroy
     has_many :items, through: :cart_items, source: :product
 
+    def find_cart_item(product)
+        cart_items.find_by(product_id: product)
+    end
+
     def add_product_to_cart(product)
     	ci = cart_items.build
         ci.product = product
