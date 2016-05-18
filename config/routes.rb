db136a3c2085e8f58a20f63682149845c7ad44e0
Ben <lyenliang@gmail.com>
2016-05-18 06:25:23 +0000

    Finish Order Form

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,8 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  resources :orders
+
   # You can have the root of your site routed with "root"
   root 'products#index'
 
