f39585a4fa0953b7a3d1703d8748a6bd398d3476
wowdyaln <rogersmk@gmail.com.tw>
2016-05-18 17:02:45 +0800

    week2HW p2

---
+++ app/controllers/cart_items_controller.rb
@@ -0,0 +1,13 @@
+class CartItemsController < ApplicationController
+
+  def destroy
+    @cart = current_cart
+    @item = @cart.cart_items.find_by(product_id: params[:id])
+    @product = @item.product
+    @item.destroy
+
+    flash[:warning] = "成功把 #{@product.title} 從購物車刪除！"
+    redirect_to :back
+  end
+
+end
