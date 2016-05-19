174175f1ae31d36c15b961ff195a942bdddc6595
Ben <lyenliang@gmail.com>
2016-05-19 03:27:26 +0000

    Users can remove a sinlge item in the cart

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,8 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  resources :items, controller: "cart_items"
+
   resources :orders do
     member do
       get :pay_with_credit_card
