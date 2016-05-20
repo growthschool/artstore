caa805947ce1001e3a02692d81744933b28a6076
Ben <lyenliang@gmail.com>
2016-05-20 03:22:01 +0000

    Fix a bug

--- app/models/order.rb
+++ app/models/order.rb
@@ -17,7 +17,7 @@ class Order < ActiveRecord::Base
     state :good_returned
     
     event :make_payment, after_commit: :pay! do
-      transitions from: :order_palced, to: :paid
+      transitions from: :order_placed, to: :paid
     end
     
     event :ship do
