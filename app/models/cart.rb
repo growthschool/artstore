168324a152d9d406ba6d25cf0cd0480b8562bf8c
dwliao <dw.liao@hotmail.com>
2016-05-22 11:13:54 +0800

    deploy to heroku

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,6 +2,10 @@ class Cart < ActiveRecord::Base
   has_many :cart_items, dependent: :destroy
   has_many :items, through: :cart_items, source: :product
 
+  def find_cart_item(product)
+    cart_items.find_by(product_id: product)
+  end
+
   def add_product_to_cart(product)
     ci = cart_items.build
     ci.product = product
@@ -11,10 +15,14 @@ class Cart < ActiveRecord::Base
   def total_price
     sum = 0
 
-    items.each do |item|
-      sum = sum + item.price
+    cart_items.each do |cart_item|
+      sum = sum + (cart_item.product.price * cart_item.quantity)
     end
 
     sum
+  end
+
+  def clean!
+    cart_items.destroy_all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,35 @@
 Rails.application.routes.draw do
+  resources :orders do
+    member do
+      get :pay_with_credit_card
+    end
+  end
+
   root "products#index"
 
-  resources :carts
+  namespace :account do
+    resources :orders
+  end
+
+  resources :items, controller: "cart_items"
+
+  resources :carts do
+    collection do
+      post :checkout
+      delete :clean
+    end
+  end
   devise_for :users
   namespace :admin do
     resources :products
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end
     resources :users do
       member do
         post :to_admin
