65022975ee9d9328f19d543cfa5cf265e738f9a3
Tien-Yi Chi <tien-yi.chi@logicsolutions.com>
2016-05-22 14:24:32 +0800

    Cart detail page

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -7,4 +7,14 @@ class Cart < ActiveRecord::Base
 		ci.product = product
 		ci.save
 	end
+
+	def total_price
+		sum = 0
+
+		items.each do |item|
+			sum = sum + item.price
+		end
+
+		sum
+	end
 end
