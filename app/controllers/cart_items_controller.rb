d7c2603563d716c660630df5a7c827f255716ce0
Tien-Yi Chi <tien-yi.chi@logicsolutions.com>
2016-05-23 01:44:36 +0800

    Implement cart_item quantity function

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -4,5 +4,25 @@ class CartItemsController < ApplicationController
 	def destroy
 		@cart = current_cart
 		@item = @cart.cart_items.find_by(product_id: params[:id])
+		@product = @item.product
+		@item.destroy
+
+		flash[:warning] = "#{@product.title} deleted successfully."
+		redirect_to :back
+	end
+
+	def update
+		@cart = current_cart
+		@item = @cart.cart_items.find_by(product_id: params[:id])
+
+		@item.update(item_params)
+
+		redirect_to carts_path
+	end
+
+	private
+
+	def item_params
+		params.require(:cart_item).permit(:quantity)
 	end
 end
