2dcd11659a90274e04248248818a479f8e3e53c8
loyuting <churain2@gmail.com>
2016-05-27 23:33:29 +0800

    第二周作業1

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,39 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @order = current_user.orders.build(order_params)
+
+    if @order.save
+      @order.build_item_cache_from_cart(current_cart)
+      @order.calculate_total!(current_cart)
+      current_cart.clean!
+      redirect_to order_path(@order.token)
+    else
+      render "carts/checkout"
+    end
+  end
+
+  def show
+    @order = Order.find_by_token(params[:id])
+    @order_info = @order.info
+    @order_items = @order.items
+  end
+
+  def pay_with_credit_card
+    @order = Order.find_by_token(params[:id])
+    @order.set_payment_with!("credit_card")
+    @order.make_payment!
+
+    redirect_to account_orders_path, notice: "成功完成付款"
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(info_attributes: [:billing_name,
+                                                    :billing_address,
+                                                    :shipping_name,
+                                                    :shipping_address] )
+  end
+end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,12 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+      current_cart.add_product_to_cart(@product)
+      flash[:notice] = "你已成功將 #{@product.title} 加入購物車"
+
+    redirect_to :back
+  end
 end
